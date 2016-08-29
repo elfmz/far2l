f41effb76f67b7e3d4c609833a6faa314002d40f
elfmz <fenix1905@tut.by>
2016-08-30 00:11:36 +0300

    fixed some terminal problems - sudo works now

--- far2l/vtshell.cpp
+++ far2l/vtshell.cpp
@@ -27,100 +27,127 @@ class VTShell
 	std::mutex _mutex;
 	VTAnsi _vta;
 	pthread_t _thread;
-
+	bool _pipes_fallback;
 	bool  _thread_state;
 	bool _thread_exiting;
 	int _fd_out;
 	int _fd_in;
-	pid_t _pid, _grp;
+	pid_t _pid;
 	int (WINAPI *_fork_proc)(int argc, char *argv[]);
 
 	bool Startup()
 	{
-		int fd_in[2], fd_out[2], r = -1;
-
-		int fdm = posix_openpt(O_RDWR); 
-		if (fdm!=-1) {
-			if (grantpt(fdm)==0 && unlockpt(fdm)==0) {
-				int fds = open(ptsname(fdm), O_RDWR); 
-				if (fds!=-1) {
-					struct termios ts = {0};
-					if (tcgetattr(fds, &ts)==0) {
-						ts.c_lflag |= ECHO | ISIG | ICANON;
-						ts.c_cc[VINTR] = 3;
-						tcsetattr( fds, TCSAFLUSH, &ts );
-					} 
-					CONSOLE_SCREEN_BUFFER_INFO csbi = {0};
-					if (WINPORT(GetConsoleScreenBufferInfo)( NULL, &csbi )) {
-						struct winsize ws = {0};
-						ws.ws_row =csbi.dwSize.Y ? csbi.dwSize.Y : 1;
-						ws.ws_col = csbi.dwSize.X ? csbi.dwSize.X : 1;
-						fprintf(stderr, "setting console size: %u x %u\n", ws.ws_col, ws.ws_row);
-						ioctl( fdm, TIOCSWINSZ, &ws );						
-					}
-					fd_in[0] = fds;
-					fd_out[1] = dup(fds);
-					fd_in[1] = fdm;
-					fd_out[0] = dup(fdm);
-					r = 0;
+		int fd_term = posix_openpt(O_RDWR | O_NOCTTY); //use -1 to verify _pipes_fallback
+		const char *slavename = NULL;
+		if (fd_term!=-1) {
+			_pipes_fallback = false;
+			if (grantpt(fd_term)==0 && unlockpt(fd_term)==0) {
+				CONSOLE_SCREEN_BUFFER_INFO csbi = { };
+				if (WINPORT(GetConsoleScreenBufferInfo)( NULL, &csbi )  
+							&& csbi.dwSize.X && csbi.dwSize.Y) {
+								
+					struct winsize ws = {(unsigned short)csbi.dwSize.Y, 
+						(unsigned short)csbi.dwSize.X, 0, 0};
+					if (ioctl( fd_term, TIOCSWINSZ, &ws )==-1)
+						perror("VT: ioctl(TIOCSWINSZ)");
 				}
+				
+				slavename = ptsname(fd_term);
+				if (!slavename)
+					perror("VT: ptsname");
+			} else
+				perror("VT: grantpt/unlockpt");
+				
+			if (!slavename) {
+				close(fd_term);
+				fd_term = -1;
 			}
-			if (r!=0)
-				close (fdm);
-		}
+		} else
+			perror("VT: posix_openpt");
 
-		if (r!=0) {
-			fprintf(stderr, "pseudo-VT failed, fallback to pipes\n");
-			r = pipe(fd_in);
-			if (r < 0) {
-				perror("VTShell: fds_in");
+		int fd_in[2] = {-1, -1}, fd_out[2] = {-1, -1};
+		if (fd_term==-1) {
+			fprintf(stderr, "VT: fallback to pipes\n");
+			if (pipe(fd_in) < 0) {
+				perror("VT: fds_in");
 				return false;
 			}
 
-			r = pipe(fd_out);
-			if (r < 0) {
-				perror("VTShell: fds_out");
+			if (pipe(fd_out) < 0) {
+				perror("VT: fds_out");
 				close_fd_pair(fd_in);
 				return false;
 			}
+			_pipes_fallback = true;
 		}
-
-		r = fork();
+		
+		int r = fork();
 		if (r == 0) {
-			setsid();
-			if ( ioctl( 0, TIOCSCTTY, 0 ) )
-				 perror( "ioctl(TIOCSCTTY)" );
+			if (fd_term!=-1) {
+				close(fd_term);
+				fd_term = -1;
+			}
+			if (slavename) {
+				if (setsid()==-1)
+					perror("VT: setsid");
+				
+				fd_term = open(slavename, O_RDWR); 
+				if (fd_term==-1) {
+					perror("VT: open slave");
+					exit(errno);
+				}
+				
+				struct termios ts = {0};
+				if (tcgetattr(fd_term, &ts)==0) {
+					ts.c_lflag |= ECHO | ISIG | ICANON;
+					ts.c_cc[VINTR] = 3;
+					tcsetattr( fd_term, TCSAFLUSH, &ts );
+				} 
+				if ( ioctl( 0, TIOCSCTTY, 0 )==-1 )
+					perror( "VT: ioctl(TIOCSCTTY)" );
+					
+				dup2(fd_term, STDIN_FILENO);
+				dup2(fd_term, STDOUT_FILENO);
+				dup2(fd_term, STDERR_FILENO);
+				close(fd_term);
+				fd_term = -1;
+			} else {
+				dup2(fd_in[0], STDIN_FILENO);
+				dup2(fd_out[1], STDOUT_FILENO);
+				dup2(fd_out[1], STDERR_FILENO);
+				close_fd_pair(fd_in); 
+				close_fd_pair(fd_out);				
+			}
+			
 			//setenv("TERM", "xterm-256color", 1);
 			setenv("TERM", "xterm", 1);
 			signal( SIGINT, SIG_DFL );
 
-			dup2(fd_in[0], STDIN_FILENO);
-			dup2(fd_out[1], STDOUT_FILENO);
-			dup2(fd_out[1], STDERR_FILENO);
-			close_fd_pair(fd_in); 
-			close_fd_pair(fd_out);
 			ExecuteOrForkProc(_cmd.c_str(), _fork_proc) ;
 		}
 	
 		if (r==-1) { 
-			perror("VTShell: fork");
-			close_fd_pair(fd_in);
-			close_fd_pair(fd_out);
+			perror("VT: fork");
+			if (fd_term==-1) {
+				close_fd_pair(fd_in);
+				close_fd_pair(fd_out);
+			} else
+				close(fd_term);
 			return false;
 		}
-	
-		close(fd_in[0]);
-		close(fd_out[1]);
-		_fd_in = fd_in[1];
-		_fd_out = fd_out[0];
+		
+		if (fd_term==-1) {
+			close(fd_in[0]);
+			close(fd_out[1]);
+			_fd_in = fd_in[1];
+			_fd_out = fd_out[0];
+		} else {
+			_fd_in = dup(fd_term);
+			_fd_out = fd_term;
+		}
 		fcntl(_fd_in, F_SETFD, FD_CLOEXEC);
 		fcntl(_fd_out, F_SETFD, FD_CLOEXEC);
 		_pid = r;		
-		for(;;){
-			_grp = getpgid(_pid);
-			if (_grp!=getpgid(getpid()))break;
-			usleep(10000);
-		}
 		return true;
 	}
 	
@@ -194,19 +221,19 @@ class VTShell
 			close(_fd_out);
 			_fd_out = -1;
 		}
-		_grp = -1;
 	}
 
 	public:
 	VTShell(const char *cmd, int (WINAPI *fork_proc)(int argc, char *argv[]) = NULL )
-		: _cmd(cmd), _thread_state(false), _thread_exiting(false), _fd_out(-1), _fd_in(-1), _pid(-1),  _grp(-1), _fork_proc(fork_proc)
+		: _cmd(cmd), _pipes_fallback(false), _thread_state(false), _thread_exiting(false), 
+		_fd_out(-1), _fd_in(-1), _pid(-1),  _fork_proc(fork_proc)
 	{
 		if (!Startup())
 			return;
 
 		_thread_state = true;
 		if (pthread_create(&_thread, NULL, sOutputReaderThread, this)) {
-			perror("VTShell: pthread_create");
+			perror("VT: pthread_create");
 			_thread_state = false;
 			Shutdown();
 		}
@@ -230,11 +257,14 @@ class VTShell
 			if (ir.EventType==KEY_EVENT && ir.Event.KeyEvent.bKeyDown) {
 				const std::string &translated = TranslateKeyEvent(ir.Event.KeyEvent);
 				if (!translated.empty()) {
+					if (_pipes_fallback && ir.Event.KeyEvent.uChar.UnicodeChar) {
+						WINPORT(WriteConsole)( NULL, &ir.Event.KeyEvent.uChar.UnicodeChar, 1, &dw, NULL );
+					}
 					if (write(_fd_in, translated.c_str(), translated.size())<=0) {
-						fprintf(stderr, "VTShell: write failed\n");
+						fprintf(stderr, "VT: write failed\n");
 					}
 				} else {
-					fprintf(stderr, "VTShell: not translated keydown: VK=0x%x MODS=0x%x char=0x%x\n", 
+					fprintf(stderr, "VT: not translated keydown: VK=0x%x MODS=0x%x char=0x%x\n", 
 						ir.Event.KeyEvent.wVirtualKeyCode, ir.Event.KeyEvent.dwControlKeyState,
 						ir.Event.KeyEvent.uChar.UnicodeChar );
 				}
@@ -246,7 +276,7 @@ class VTShell
 
 		int status = 0;
 		if (waitpid(_pid, &status, 0)==-1) {
-			fprintf(stderr, "VTShell: waitpid(0x%x) error %u\n", _pid, errno);
+			fprintf(stderr, "VT: waitpid(0x%x) error %u\n", _pid, errno);
 			status = 1;
 		}
 		return status;
@@ -268,29 +298,35 @@ class VTShell
 	std::string TranslateKeyEvent(const KEY_EVENT_RECORD &KeyEvent)
 	{
 		if (KeyEvent.wVirtualKeyCode) {
-			if (KeyEvent.wVirtualKeyCode==VK_CONTROL || KeyEvent.wVirtualKeyCode==VK_MENU || KeyEvent.wVirtualKeyCode==VK_SHIFT)
-				return std::string();
-				
 			const bool ctrl = (KeyEvent.dwControlKeyState & (LEFT_CTRL_PRESSED|RIGHT_CTRL_PRESSED)) != 0;
 			const bool alt = (KeyEvent.dwControlKeyState & (RIGHT_ALT_PRESSED|LEFT_ALT_PRESSED)) != 0;
 			const bool shift = (KeyEvent.dwControlKeyState & (SHIFT_PRESSED)) != 0;
 			if (ctrl && !shift && KeyEvent.wVirtualKeyCode=='C') {
-				SendSignal(alt ? SIGKILL : SIGINT);
+				if (alt) {
+					fprintf(stderr, "VT: Ctrl+Alt+C - killing them hardly...\n");
+					SendSignal(SIGKILL);
+				} else if (_pipes_fallback) 
+					SendSignal(SIGINT);
 			}
-		
+				
 			const char *spec = VT_TranslateSpecialKey(KeyEvent.wVirtualKeyCode, ctrl, alt, shift);
 			if (spec)
 				return spec;
 		}
 
-		wchar_t wz[2] = {KeyEvent.uChar.UnicodeChar, 0};
+		wchar_t wz[3] = {KeyEvent.uChar.UnicodeChar, 0};
+		if (_pipes_fallback && wz[0] == '\r') 
+			wz[0] = '\n';
+		
 		return Wide2MB(&wz[0]);
 	}
 	
 	void SendSignal(int sig)
 	{
-		if (_grp!=-1)
-			killpg(_grp, sig);
+		pid_t grp = getpgid(_pid);
+		
+		if (grp!=-1 && grp!=getpgid(getpid()))
+			killpg(grp, sig);
 		else
 			kill(_pid, sig);
 	}

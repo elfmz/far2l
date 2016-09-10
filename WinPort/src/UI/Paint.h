b3a4f08f1d06b0441e56147175670aa363209d6a
elfmz <fenix1905@tut.by>
2016-09-10 18:56:07 +0300

    less memory reallocations on painting

--- WinPort/src/UI/Paint.h
+++ WinPort/src/UI/Paint.h
@@ -11,7 +11,8 @@ class ConsolePaintContext
 		std::vector<uint8_t> result;
 	} _char_fit_cache;
 
-	std::vector<CHAR_INFO> _line;	
+	std::vector<CHAR_INFO> _line;
+	wxString _buffer;
 	
 public:
 	ConsolePaintContext(wxWindow *window);
@@ -61,13 +62,13 @@ class ConsolePainter
 	} _brush;
 	
 	
-	wxString _buffer;
 	ConsolePaintContext *_context;
 	wxPaintDC &_dc;
 	CursorProps _cursor_props;
 	unsigned int _start_cx, _start_cy, _start_back_cx;
 	unsigned int _start_x, _start_y;
 	wxColour _clr_text, _clr_back;
+	wxString &_buffer;
 	
 	void SetBackgroundColor(wxColour clr);
 	void PrepareBackground(unsigned int cx, wxColour clr);
@@ -75,7 +76,7 @@ class ConsolePainter
 	void FlushText();
 		
 public:
-	ConsolePainter(ConsolePaintContext *context, wxPaintDC &dc);
+	ConsolePainter(ConsolePaintContext *context, wxPaintDC &dc, wxString &_buffer);
 	
 
 	void NextChar(unsigned int cx, unsigned short attributes, wchar_t c);

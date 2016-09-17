6e31aa62cfa70df59d36ae3c4172f677da672fd8
elfmz <fenix1905@tut.by>
2016-09-17 17:56:19 +0300

    v-block copy-paste now uses system clipboard

--- WinPort/src/APIClipboard.cpp
+++ WinPort/src/APIClipboard.cpp
@@ -22,9 +22,13 @@
 #include "WinPortHandle.h"
 #include "PathHelpers.h"
 #include "CallInMain.h"
+#include "utils.h"
 
 extern "C" {
-	static class CustomFormats : std::map<UINT, std::wstring>  {
+	static class CustomFormats : std::map<UINT, wxDataFormat>
+	{
+		UINT _next_index;
+
 	public:
 		CustomFormats() :_next_index(0)
 		{
@@ -32,8 +36,9 @@ extern "C" {
 
 		UINT Register(LPCWSTR lpszFormat)
 		{
-			for (auto i : *this) {
-				if (i.second == lpszFormat)
+			wxString format(lpszFormat);
+			for (const auto &i : *this) {
+				if (i.second.GetId() == format)
 					return i.first;
 			}
 
@@ -42,20 +47,23 @@ extern "C" {
 				if (_next_index < 0xC000 || _next_index > 0xFFFF)
 					_next_index = 0xC000;
 				if (find(_next_index)==end()) {
-					insert(value_type(_next_index, std::wstring(lpszFormat)));
+					insert(value_type(_next_index, wxDataFormat(format)));
 					return _next_index;
 				}
 			}
 		}
+		
+		const wxDataFormat *Lookup(UINT v) const
+		{
+			const_iterator i = find(v);
+			return ( i == end() ) ? nullptr : &i->second;
+		}
 
 
-		UINT _next_index;
 	} g_custom_formats;
 
-
-	static struct CustomData : std::map<UINT, std::vector<char> >  {
-	} g_custom_data;
 	static std::set<PVOID> g_free_pendings;
+	static wxDataObjectComposite *g_data_to_clipboard = nullptr;
 	
 	WINPORT_DECL(RegisterClipboardFormat, UINT, (LPCWSTR lpszFormat))
 	{		
@@ -105,6 +113,11 @@ extern "C" {
 
 		for (auto p : g_free_pendings) free(p);
 		g_free_pendings.clear();
+		
+		if (g_data_to_clipboard) {
+			wxTheClipboard->SetData( g_data_to_clipboard );
+			g_data_to_clipboard = nullptr;
+		}
 		wxTheClipboard->Flush();
 		wxTheClipboard->Close();
 		
@@ -114,15 +127,18 @@ extern "C" {
 		
 		return TRUE;
 	}
+
 	WINPORT_DECL(EmptyClipboard, BOOL, ())
 	{
 		if (!wxIsMainThread()) {
 			auto fn = std::bind(WINPORT(EmptyClipboard));
 			return CallInMain<BOOL>(fn);
 		}
+		
 		fprintf(stderr, "EmptyClipboard\n");
+		delete g_data_to_clipboard;
+		g_data_to_clipboard = nullptr;
 		wxTheClipboard->Clear();
-		g_custom_data.clear();
 		return TRUE;
 	}
 
@@ -132,36 +148,64 @@ extern "C" {
 			auto fn = std::bind(WINPORT(IsClipboardFormatAvailable), format);
 			return CallInMain<BOOL>(fn);
 		}
-		if (format!=CF_UNICODETEXT && format!=CF_TEXT) {
-			CustomData::iterator i = g_custom_data.find(format);
-			return (i==g_custom_data.end()) ? FALSE : TRUE;
-		} else {
+		
+		if (format==CF_UNICODETEXT || format==CF_TEXT) {
 			return wxTheClipboard->IsSupported( wxDF_TEXT ) ? TRUE : FALSE;
+		} else {
+			const wxDataFormat *data_format = g_custom_formats.Lookup(format);
+			if (!data_format) {
+				fprintf(stderr, "IsClipboardFormatAvailable(%u) - unrecognized format\n", format);
+				return FALSE;
+			}
+			
+			return wxTheClipboard->IsSupported(*data_format) ? TRUE : FALSE;
 		}
 	}
-
+	
 	WINPORT_DECL(GetClipboardData, HANDLE, (UINT format))
 	{
 		if (!wxIsMainThread()) {
 			auto fn = std::bind(WINPORT(GetClipboardData), format);
 			return CallInMain<HANDLE>(fn);
 		}
-		PVOID p = NULL;		
-		if (format==CF_UNICODETEXT) {
+		
+		PVOID p = nullptr;		
+		if (format==CF_UNICODETEXT || format==CF_TEXT) {
 			wxTextDataObject data;
-			wxTheClipboard->GetData( data );
-			p = _wcsdup(data.GetText().wchar_str());
-		} else if (format==CF_TEXT) {
-			wxTextDataObject data;
-			wxTheClipboard->GetData( data );
-			p = _strdup(data.GetText().char_str());
+			if (!wxTheClipboard->GetData( data ))
+				return nullptr;
+				
+			p = (format==CF_UNICODETEXT) ? 
+				(void *)_wcsdup(data.GetText().wchar_str()) : 
+				(void *)_strdup(data.GetText().char_str());
 		} else {
-			CustomData::iterator i = g_custom_data.find(format);
-			if (i==g_custom_data.end()) 
-				return NULL;
-
-			p = malloc(i->second.size());
-			if (p) memcpy(p, &i->second[0], i->second.size());
+			const wxDataFormat *data_format = g_custom_formats.Lookup(format);
+			if (!data_format) {
+				fprintf(stderr, "GetClipboardData(%u) - not registered format\n", format);
+				return nullptr;
+			}
+			
+			if (!wxTheClipboard->IsSupported(*data_format)) {
+				//fprintf(stderr, "GetClipboardData(%s) - not supported format\n", 
+				//	(const char *)data_format->GetId().char_str());
+				return nullptr;
+			}
+			
+			wxCustomDataObject data(*data_format);
+			if (!wxTheClipboard->GetData( data )) {
+				fprintf(stderr, "GetClipboardData(%s) - GetData failed\n", 
+					(const char *)data.GetFormat().GetId().char_str());
+				return nullptr;
+			}
+				
+			const size_t data_size = data.GetDataSize();
+			p = calloc(data_size + 1, 1); 
+			if (!p) {
+				fprintf(stderr, "GetClipboardData(%s) - cant alloc %u + 1\n", 
+					(const char *)data_format->GetId().char_str(), (unsigned int)data_size);
+				return nullptr;
+			}
+			data.GetDataHere(p);
 		}
 
 		if (p) {
@@ -170,12 +214,14 @@ extern "C" {
 		return p;
 	}
 
+
 	WINPORT_DECL(SetClipboardData, HANDLE, (UINT format, HANDLE mem))
 	{
 		if (!wxIsMainThread()) {
 			auto fn = std::bind(WINPORT(SetClipboardData), format, mem);
 			return CallInMain<HANDLE>(fn);
 		}
+		
 #ifdef _WIN32
 		size_t len = _msize(mem);
 #elif defined(__APPLE__)
@@ -184,14 +230,24 @@ extern "C" {
 		size_t len = malloc_usable_size(mem);
 #endif
 		fprintf(stderr, "SetClipboardData: %u\n", format);
+		if (!g_data_to_clipboard) {
+			g_data_to_clipboard = new wxDataObjectComposite;
+		}
 		if (format==CF_UNICODETEXT) {
-			wxTheClipboard->SetData( new wxTextDataObject((const wchar_t *)mem) );
+			g_data_to_clipboard->Add(new wxTextDataObject((const wchar_t *)mem), true);
 		} else if (format==CF_TEXT) {
-			wxTheClipboard->SetData( new wxTextDataObject((const char *)mem) );
+			g_data_to_clipboard->Add(new wxTextDataObject((const char *)mem));
 		} else {
-			std::vector<char> &data = g_custom_data[format];
-			data.resize(len);
-			if (len) memcpy(&data[0], mem, data.size());			
+			const wxDataFormat *data_format = g_custom_formats.Lookup(format);
+			if (!data_format) {
+				fprintf(stderr, 
+					"SetClipboardData(%u, %p [%u]) - unrecognized format\n", 
+					format, mem, (unsigned int)len);
+			} else {
+				wxCustomDataObject *dos = new wxCustomDataObject(*data_format);
+				dos->SetData(len, mem);		
+				g_data_to_clipboard->Add(dos);
+			}
 		}
 
 		if (mem) {

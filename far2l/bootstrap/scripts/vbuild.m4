d78a1996d3627b910becf2076d3a563631b926c0
elfmz <fenix1905@tut.by>
2016-10-17 02:40:44 +0300

    build string is autogerenated now during clean rebuild

--- far2l/bootstrap/scripts/vbuild.m4
+++ far2l/bootstrap/scripts/vbuild.m4
@@ -1,1 +1,1 @@
-m4_define(BUILD,16/10/16-3d5b183-alpha)m4_dnl
+m4_define(BUILD, m4_esyscmd(./vbuild.sh |tr -d '\n'))m4_dnl
--- update_build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo "m4_define(BUILD,$(date +"%y/%m/%d-")$(git rev-parse --short HEAD)-alpha)m4_dnl" > ./far2l/bootstrap/scripts/vbuild.m4

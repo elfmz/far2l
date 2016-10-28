b43c69d72e83caeaa97764b7741f01aa4790b35c
elfmz <fenix1905@tut.by>
2016-10-29 02:14:27 +0300

    open.sh and trash.sh per user customization

--- far2l/bootstrap/open.sh
+++ far2l/bootstrap/open.sh
@@ -2,13 +2,22 @@
 ############################################################
 #This script used by FAR to open files by other applications
 ############################################################
-#$1= [exec|dir|other] wher:
+#$1= [exec|dir|other] where:
 #  exec - execute given command in other terminal
-#  dir - open given directory witg GUI
-#  other - open given file witg GUI
+#  dir - open given directory with GUI
+#  other - open given file with GUI
+#Other arguments - actual command/file to be executed/opened
+############################################################
+#For per user customization - create:
+#~/.config/far2l/open.sh
+############################################################
 
 what=$1
 shift
+
+if [ -x ~/.config/far2l/open.sh ]; then
+. ~/.config/far2l/open.sh
+fi
 
 if [ "$what" = "exec" ]; then
 	if command -v xterm >/dev/null 2>/dev/null ; then
--- far2l/bootstrap/trash.sh
+++ far2l/bootstrap/trash.sh
@@ -2,6 +2,14 @@
 ##########################################################
 #This script used by FAR to move files to Trash
 ##########################################################
+#For per user customization - create:
+#~/.config/far2l/trash.sh
+##########################################################
 
 set -e
+
+if [ -x ~/.config/far2l/trash.sh ]; then
+. ~/.config/far2l/trash.sh
+fi
+
 gvfs-trash -f "$1"

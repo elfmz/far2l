5d2cb16a8a8dfbe20050d7ae09dc03b27a70a5d1
elfmz <fenix1905@tut.by>
2018-10-07 22:32:03 +0300

    colors&chars

--- WinPort/src/Backend/TTY/TTYWriter.h
+++ WinPort/src/Backend/TTY/TTYWriter.h
@@ -5,6 +5,20 @@
 class TTYWriter
 {
 	unsigned int _y = -1, _x = -1;
+	struct Attributes
+	{
+		Attributes() = default;
+		Attributes(const Attributes &) = default;
+		Attributes(WORD attributes);
+
+		bool bold = false;
+		bool underline = false;
+		unsigned char foreground = -1;
+		unsigned char background = -1;
+
+		bool operator ==(const Attributes &attr) const;
+		bool operator !=(const Attributes &attr) const {return !(operator ==(attr)); }
+	} _attr;
 
 	std::vector<char> _rawbuf;
 	bool WriteRaw(const char *str, int len);

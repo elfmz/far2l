4f64041b1fbd3b3b349ca42099ebdd4fa25ccb0c
elfmz <fenix1905@tut.by>
2018-10-09 01:23:46 +0300

    .

--- WinPort/src/Backend/TTY/TTYInput.cpp
+++ WinPort/src/Backend/TTY/TTYInput.cpp
@@ -2,18 +2,48 @@
 #include "ConsoleInput.h"
 #include "ConvertUTF.h"
 #include "WinPort.h"
+#include <utils.h>
+#include <VTTranslation.h>
 
 extern ConsoleInput g_winport_con_in;
 
-void TTYInput::PostSimpleKeyEvent(wchar_t ch)
+static bool IsEnhancedKey(WORD code)
+{
+	return (code==VK_LEFT || code==VK_RIGHT || code==VK_UP || code==VK_DOWN
+		|| code==VK_HOME || code==VK_END);// || code==VK_PAGEDOWN || code==VK_PAGEUP );
+}
+
+
+TTYInput::TTYInput()
+{
+	for (int controls = 0; controls < 4; ++controls) { // 0 ctrl alt shift
+	for (WORD key_code = 1; key_code <= 0xff; ++key_code) {
+
+		const char *csi = VT_TranslateSpecialKey(key_code, controls == 1, controls == 2, controls == 3, 0);
+		if (csi != nullptr && *csi == 0x1b) {
+			Key k = {};
+			k.key_code = key_code;
+			switch (controls) {
+				case 1: k.control_keys = LEFT_CTRL_PRESSED; break;
+				case 2: k.control_keys = LEFT_ALT_PRESSED; break;
+				case 3: k.control_keys = SHIFT_PRESSED; break;
+			}
+			if (IsEnhancedKey(key_code))
+				k.control_keys = ENHANCED_KEY;
+			_csi2key.emplace(csi, k);
+		}
+
+	} }
+}
+
+void TTYInput::PostCharEvent(wchar_t ch)
 {
 	INPUT_RECORD ir = {};
 	ir.EventType = KEY_EVENT;
 	ir.Event.KeyEvent.bKeyDown = TRUE;
 	ir.Event.KeyEvent.wRepeatCount = 1;
 	ir.Event.KeyEvent.uChar.UnicodeChar = ch;
-	ir.Event.KeyEvent.wVirtualKeyCode = ch;
-	ir.Event.KeyEvent.wVirtualScanCode = WINPORT(MapVirtualKey)(ch, MAPVK_VK_TO_VSC);
+	ir.Event.KeyEvent.wVirtualKeyCode = VK_OEM_PERIOD;
 	g_winport_con_in.Enqueue(&ir, 1);
 	ir.Event.KeyEvent.bKeyDown = FALSE;
 	g_winport_con_in.Enqueue(&ir, 1);
@@ -26,12 +56,12 @@ bool TTYInput::BufParseIterationSimple()
 	ConvertUTF8toUTF32 ( &utf8_start, utf8_start + _buf.size(),
 		&utf32_start, utf32_start + 1, lenientConversion);
 	if (utf32_start != &utf32[0]) {
-		PostSimpleKeyEvent(utf32[0]);
+		PostCharEvent(utf32[0]);
 		_buf.erase(_buf.begin(), _buf.begin() + (utf8_start - (const UTF8*)&_buf[0]) );
 		return true;
 
 	} else if (_buf.size() > 6) {
-		fprintf(stderr, "Failed to decode UTF8 sequence: 0x%x\n", _buf[0]);
+		fprintf(stderr, "Failed to decode UTF8 sequence: 0x%x '%c'\n", _buf[0], _buf[0]);
 		_buf.erase(_buf.begin());
 		return true;
 	}
@@ -41,8 +71,39 @@ bool TTYInput::BufParseIterationSimple()
 
 bool TTYInput::BufParseIterationCSI()
 {
-	_buf.erase(_buf.begin());//TODO
-	return true;
+//	abort();
+	// TODO: optimized lookup
+	size_t max_len = 0;
+	for (const auto &i : _csi2key) {
+		if (_buf.size() >= i.first.size() && memcmp(&_buf[0], i.first.c_str(), i.first.size()) == 0) {
+//			fprintf(stderr, "RECOGNIZED: '%s'\n", i.first.c_str() + 1);
+
+			_buf.erase(_buf.begin(), _buf.begin() + i.first.size());
+
+			INPUT_RECORD ir = {};
+			ir.EventType = KEY_EVENT;
+			ir.Event.KeyEvent.wRepeatCount = 1;
+//			ir.Event.KeyEvent.uChar.UnicodeChar = i.second.unicode_char;
+			ir.Event.KeyEvent.wVirtualKeyCode = i.second.key_code;
+			ir.Event.KeyEvent.wVirtualScanCode = WINPORT(MapVirtualKey)(i.second.key_code,MAPVK_VK_TO_VSC);
+
+			ir.Event.KeyEvent.bKeyDown = TRUE;
+			g_winport_con_in.Enqueue(&ir, 1);
+			ir.Event.KeyEvent.bKeyDown = FALSE;
+			g_winport_con_in.Enqueue(&ir, 1);
+
+			return true;
+		}
+		if (max_len < i.first.size())
+			max_len = i.first.size();
+	}
+
+	if (_buf.size() >= max_len) {
+		_buf.erase(_buf.begin());//TODO
+		return true;
+	}
+
+	return false;
 }
 
 void TTYInput::OnBufUpdated()
---
+++ utils/include/VTTranslation.h
@@ -0,0 +1,3 @@
+#pragma once
+
+const char *VT_TranslateSpecialKey(const WORD key, bool ctrl, bool alt, bool shift, unsigned char keypad);

aff5a6d09cab256f331db2aaefd7bb6fd3cb5419
elfmz <fenix1905@tut.by>
2019-05-09 23:44:16 +0300

    allow once/allow always/cancel choice on server identity change

--- NetRocks/src/UI/ConfirmNewServerIdentity.h
+++ NetRocks/src/UI/ConfirmNewServerIdentity.h
@@ -6,10 +6,16 @@
 
 class ConfirmNewServerIdentity : protected BaseDialog
 {
-	int _i_ok = -1;
+	int _i_allow_once = -1, _i_allow_always = -1;
 
 public:
+	enum Result
+	{
+		R_DENY = 0,
+		R_ALLOW_ONCE,
+		R_ALLOW_ALWAYS
+	};
 	ConfirmNewServerIdentity(const std::string &site, const std::string &identity);
 
-	bool Ask();
+	Result Ask();
 };

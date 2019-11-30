30d62bea414ecc0ca9eb68270dede13935ad9862
elfmz <elfmz@nowhere>
2019-12-01 00:08:56 +0300

    cygwin: VT clipboard UTF16/32 interoperability (fix #640)

--- WinPort/src/Backend/TTY/TTYFar2lClipboardBackend.cpp
+++ WinPort/src/Backend/TTY/TTYFar2lClipboardBackend.cpp
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <utils.h>
 #include <base64.h>
+#include <ConvertUTF.h>
 #include "TTYFar2lClipboardBackend.h"
 #include "FSClipboardBackend.h"
 
@@ -147,8 +148,30 @@ void *TTYFar2lClipboardBackend::OnClipboardSetData(UINT format, void *data)
 
 	try {
 		StackSerializer stk_ser;
+
+#if (__WCHAR_MAX__ <= 0xffff)
+		UTF32 *new_data = nullptr;
+		if (format == CF_UNICODETEXT && len != 0) { // UTF16 -> UTF32
+			int cnt = 0;
+			const UTF16 *src = (const UTF16 *)data;
+			CalcSpaceUTF16toUTF32(&cnt, &src, src + len / sizeof(UTF16), lenientConversion);
+			new_data = (UTF32 *)malloc((cnt + 1) * sizeof(UTF32));
+			if (new_data != nullptr) {
+				new_data[cnt] = 0;
+				src = (const UTF16 *)data;
+				UTF32 *dst = new_data;
+				ConvertUTF16toUTF32( &src, src + len / sizeof(UTF16), &dst, dst + cnt, lenientConversion);
+				len = cnt * sizeof(UTF32);
+			}
+		}
+		stk_ser.Push(new_data ? new_data : data, len);
+		stk_ser.PushPOD(len);
+		free(new_data);
+#else
 		stk_ser.Push(data, len);
 		stk_ser.PushPOD(len);
+#endif
+
 		stk_ser.PushPOD(format);
 		stk_ser.PushPOD('s');
 		Far2lInterract(stk_ser, true);
@@ -176,6 +199,24 @@ void *TTYFar2lClipboardBackend::OnClipboardGetData(UINT format)
 			data = malloc(len);
 			if (data) {
 				stk_ser.Pop(data, len);
+#if (__WCHAR_MAX__ <= 0xffff)
+				if (format == CF_UNICODETEXT) { // UTF32 -> UTF16
+					int cnt = 0;
+					const UTF32 *src = (const UTF32 *)data;
+					CalcSpaceUTF32toUTF16(&cnt, &src, src + len / sizeof(UTF32), lenientConversion);
+					UTF16 *new_data = (UTF16 *)malloc((cnt + 1) * sizeof(UTF16));
+					if (new_data != nullptr) {
+						new_data[cnt] = 0;
+						src = (const UTF32 *)data;
+						UTF16 *dst = new_data;
+						ConvertUTF32toUTF16( &src, src + len / sizeof(UTF32), &dst, dst + cnt, lenientConversion);
+						free(data);
+						data = new_data;
+						len = cnt * sizeof(UTF16);
+					}
+				}
+#endif
+
 				return data;
 			}
 		}

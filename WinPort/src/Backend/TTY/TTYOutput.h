dfd9afdadb2f95dd59399c4a1306f42177e357de
elfmz <fenix1905@tut.by>
2020-09-27 02:39:11 +0300

    TTY backend: minor optimization

--- WinPort/src/Backend/TTY/TTYOutput.h
+++ WinPort/src/Backend/TTY/TTYOutput.h
@@ -42,7 +42,8 @@ public:
 	void Flush();
 
 	void ChangeCursor(bool visible, bool force = false);
-	void MoveCursor(unsigned int y, unsigned int x, bool force = false);
+	inline bool ShouldMoveCursor(unsigned int y, unsigned int x) const { return x != _cursor.x || y != _cursor.y; }
+	void MoveCursor(unsigned int y, unsigned int x);
 	void WriteLine(const CHAR_INFO *ci, unsigned int cnt);
 	void ChangeKeypad(bool app);
 	void ChangeMouse(bool enable);

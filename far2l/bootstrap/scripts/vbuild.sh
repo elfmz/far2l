435ecef649ba45bd734fca4d8b14ea5d127f3edd
cycleg <golovin.alexei@gmail.com>
2020-12-10 20:50:36 +0300

    Packaging into DEB (#853)
    * Полировка vbuild.sh.
    
    * Переработано создание DEB и TGZ пакетов.
    
    * Добавлены условные зависимости DEB-пакета из главного проектного файла.
    
    * Еще полировка vbuild.sh.

--- far2l/bootstrap/scripts/vbuild.sh
+++ far2l/bootstrap/scripts/vbuild.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
-if command -v git >/dev/null 2>/dev/null ; then
+set +e
+
+git status >/dev/null 2>/dev/null
+res=$?
+if [ $res -eq 0 ] ; then
 	git log -1 --date=format:'%y/%m/%d' --format=%cd-%h
 else
 	while :
 	do
 		if [ -f .git/refs/heads/master ]; then
-			cat .git/refs/heads/master
+			echo "$(date +%y/%m/%d)-$(cat .git/refs/heads/master)"
 			break
 		fi
 		BEFORE_CD=`pwd`
 		cd ..
 		AFTER_CD=`pwd`
 		if [ "$BEFORE_CD" = "$AFTER_CD" ]; then
-			echo unknown
+			echo "$(date +%y/%m/%d)-unknown"
 			break
 		fi
-	
 	done
 fi

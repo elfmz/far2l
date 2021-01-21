70f2d7c08da565b4f3bb15a1047894027ac22436
elfmz <fenix1905@tut.by>
2021-01-21 22:53:33 +0300

    simplify closewait.sh script

--- far2l/bootstrap/closewait.sh
+++ far2l/bootstrap/closewait.sh
@@ -13,27 +13,19 @@ if [ "$1" = "--delete" ]; then
 fi
 path=$1
 
-if command -v cut  >/dev/null 2>&1 && \
-		command -v grep >/dev/null 2>&1 && \
-		command -v lsof >/dev/null 2>&1 ; then
-#	leading_char="$(echo "$path" | cut -c 1)"
-#	if [ $leading_char = "." ]; then
-#		path="$(pwd)$(echo "$path" | cut -c 2-)"
-#	elif [ $leading_char != "/" ]; then
-#		path="$(pwd)/$path"
-#	fi
+if command -v lsof >/dev/null 2>&1 ; then
 	# give app 3 seconds to open file
 	sleep 3
 	if [ -f "$path" ]; then
 		echo "Waiting for: $path"
-		while lsof | grep -q "$path" ; do
+		while lsof "$path" >/dev/null 2>&1 ; do
 			sleep 1
 		done
 	else
 		echo "Not a file: $path"
 	fi
 else
-	echo "wait: requires 'cut' 'grep' and 'lsof' utilities to be installed" >&2
+	echo "wait: requires 'lsof' utility to be installed" >&2
 	# fallback to 5 seconds sleep
 	sleep 5
 fi

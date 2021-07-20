803d2bca389de8523885f3425ee9aa750ebe17b4
elfmz <fenix1905@tut.by>
2021-07-20 21:54:03 +0300

    fixes for recent changes

--- far2l/hilight.hpp
+++ far2l/hilight.hpp
@@ -57,12 +57,8 @@ struct HighlightDataColor
 	DWORD MarkChar;
 };
 
-class Highlighter;
-
 class HighlightFiles
 {
-	friend class HighlightFilesChunk;
-
 	private:
 		TPointerArray<FileFilterParams> HiData;
 		int FirstCount, UpperCount, LowerCount, LastCount;

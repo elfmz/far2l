90ed78a72f87daec36c5a84032e446f4da27c9e7
elfmz <fenix1905@tut.by>
2021-08-12 22:27:24 +0300

    validate permissions to settings and temp directories to mitigate problems like #1052 (#1055)

--- utils/src/InMy.cpp
+++ utils/src/InMy.cpp
@@ -1,34 +1,145 @@
 #include "utils.h"
 #include <WinCompat.h>
 #include <WinPort.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
 #include <pwd.h>
-
 #include <assert.h>
-#include <mutex>
 #include <fcntl.h>
 #include "ConvertUTF.h"
 #include <errno.h>
 
+enum PrivacyLevel
+{
+	// if dir exists and accessible - don't care who owns it, if not exists - create accessible for all (0777)
+	PL_ALL = 0,
+
+	// if dir exists and accessible - don't care who owns it, if not exists - create accessible to current user only
+	PL_ANY,
+
+	// fail if dir owned by nor current user nor root
+	PL_PRIVATE
+};
+
+static bool EnsureDir(const char *dir, PrivacyLevel pl)
+{
+	struct stat s{};
+	for (int i = 0; stat(dir, &s) == -1; ++i) {
+		if (i > 10) {
+			fprintf(stderr, "%s('%s', %u) dir-pear; stat errno=%u\n", __FUNCTION__, dir, pl, errno);
+			return false;
+		}
+		if (mkdir(dir, (pl == PL_ALL) ? 0777 : 0700) == 0) {
+			return true;
+		}
+		if (errno != EEXIST) {
+			return false;
+		}
+		usleep(i * 1000);
+	}
+
+	if ((s.st_mode & S_IFMT) != S_IFDIR) {
+		fprintf(stderr, "%s('%s', %u) its not a dir; mode=0%o\n", __FUNCTION__, dir, pl, s.st_mode);
+		return false;
+	}
+
+	const auto uid = geteuid();
+	if (pl >= PL_PRIVATE && uid != s.st_uid && s.st_uid != 0) {
+		fprintf(stderr, "%s('%s', %u) uid=%u but st_uid=%u\n", __FUNCTION__, dir, pl, uid, s.st_uid);
+		return false;
+	}
+
+	if ( ((s.st_mode & S_IWOTH) != 0)
+	  || ((s.st_mode & S_IWUSR) != 0 && s.st_uid == uid)
+	  || ((s.st_mode & S_IWGRP) != 0 && s.st_gid == getegid())) {
+		return true;
+	}
+
+	// may be mode bits lie us? check it with stick
+	std::string check_path = dir;
+	check_path+= "/.stick-check.far2l";
+	int fd = open(check_path.c_str(), O_CREAT | O_RDWR | O_CLOEXEC, 0600);
+	if (fd != -1) {
+		close(fd);
+		unlink(check_path.c_str());
+		fprintf(stderr, "%s('%s', %u) file allowed; mode=0%o\n", __FUNCTION__, dir, pl, s.st_mode);
+		return true;
+	}
+
+	return false;
+}
+
+static std::string GetTempSubdirUncached(const char *what)
+{
+	std::string out;
+	std::string env_cache = what;
+	env_cache+= "_tmp";
+	const char *env = getenv(env_cache.c_str());
+	if (env && *env) {
+		out = env;
+		fprintf(stderr, "%s: '%s' cached as '%s'\n", __FUNCTION__, env_cache.c_str(), out.c_str());
+		return out;
+	}
+
+	env = getenv("TMPDIR");
+	if (env && *env == '/' && EnsureDir(env, PL_ALL)) {
+		out = env;
+		if (out.back() != '/') {
+			out+= '/';
+		}
+
+	} else if (EnsureDir("/tmp", PL_ALL)) {
+		out = "/tmp/";
+
+	} else if (EnsureDir("/var/tmp", PL_ALL)) {
+		out = "/var/tmp/";
+
+	} else {
+		perror("Can't find temp!");
+		out = "/";
+	}
+
+	out+= what;
+	out+= '_';
+
+	const size_t base_len = out.size();
+	const unsigned long long uid = geteuid();
+
+	for (unsigned int i = 0; i < 0x10000; ++i) {
+		char subdir[128];
+		if (i == 0x1000) {
+			srand(time(NULL) ^ (getpid() << 8));
+		}
+		sprintf(subdir, "%llx_%x", uid, (i < 0x1000) ? i : i | ((rand() & 0xffff) << 16) );
+
+		out.replace(base_len, out.size() - base_len, subdir);
+
+		if (EnsureDir(out.c_str(), PL_PRIVATE)) {
+			break;
+		}
+	}
+
+	setenv(env_cache.c_str(), out.c_str(), 1);
+	fprintf(stderr, "%s: '%s' inited as '%s'\n", __FUNCTION__, env_cache.c_str(), out.c_str());
+	return out;
+}
 
 static std::string GetMyHomeUncached()
 {
-	std::string out;
-
-	struct passwd *pw = getpwuid(getuid());
-	if (pw && pw->pw_dir && *pw->pw_dir) {
-		out = pw->pw_dir;
-	} else {
-		const char *env = getenv("HOME");
-		if (env && *env) {
-			out = env;
-		} else {
-			out = "/tmp";
-		}
+	struct passwd *pw = getpwuid(geteuid());
+	if (pw && pw->pw_dir && *pw->pw_dir && EnsureDir(pw->pw_dir, PL_ANY)) {
+		return std::string(pw->pw_dir);
 	}
-	return out;
+
+	const char *env = getenv("HOME");
+	if (env && EnsureDir(env, PL_ANY)) {
+		return std::string(env);
+	}
+
+	// fallback to in-temp location
+	return GetTempSubdirUncached("far2l_home");
 }
 
 
@@ -89,7 +200,7 @@ static std::string InProfileSubdir(const char *what, const char *subpath, bool c
 		struct stat s;
 		if (stat(path.substr(0, p).c_str(), &s) == -1) {
 			for (size_t i = 1; i <= p; ++i) if (path[i] == GOOD_SLASH) {
-				mkdir(path.substr(0, i).c_str(), 0700);
+				EnsureDir(path.substr(0, i).c_str(), PL_PRIVATE);
 			}
 		}
 	}
@@ -107,66 +218,15 @@ std::string InMyCache(const char *subpath, bool create_path)
 	return InProfileSubdir(".cache", subpath, create_path);
 }
 
-
-static std::string s_ready_temp;
-static std::mutex s_ready_temp_mutex;
-
-static void GetTempRoot(std::string &path)
-{
-	struct stat s;
-	std::lock_guard<std::mutex> lock(s_ready_temp_mutex);
-	if (!s_ready_temp.empty()) {
-		path = s_ready_temp;
-		return;
-	}
-
-	const char *temp = getenv("TMPDIR ");
-	if (!temp || stat(temp, &s) == -1 || (s.st_mode & S_IFMT) != S_IFDIR) {
-		temp = "/tmp";
-		if (stat(temp, &s) == -1 || (s.st_mode & S_IFMT) != S_IFDIR) {
-			temp = "/var/tmp";
-			if (stat(temp, &s) == -1 || (s.st_mode & S_IFMT) != S_IFDIR) {
-				static std::string s_in_prof_temp = InMyConfig("tmp");
-				temp = s_in_prof_temp.c_str();
-				if (stat(temp, &s) == -1 || (s.st_mode & S_IFMT) != S_IFDIR) {
-					perror("Can't get temp!");
-					return;
-				}
-			}
-		}
-	}
-
-	for (unsigned int i = 0; ; ++i )  {
-		path = temp;
-		path+= GOOD_SLASH;
-		char buf[128];
-		sprintf(buf, "far2l_%llx_%u", (unsigned long long)geteuid(), i);
-		path+= buf;
-		mkdir(path.c_str(), 0700);
-		if (stat(path.c_str(), &s) == 0 && 
-			(s.st_mode & S_IFMT) == S_IFDIR && 
-			s.st_uid == geteuid()) {
-			break;
-		}
-
-		if (i == (unsigned int)-1) {
-			perror("Can't init temp!");
-			return;
-		}
-	}
-	s_ready_temp = path;
-	
-}
-
 std::string InMyTemp(const char *subpath)
 {
-	std::string path;
-	GetTempRoot(path);
+	static std::string temp(GetTempSubdirUncached("far2l"));
+	std::string path = temp;
 	path+= GOOD_SLASH;
 	if (subpath) {
 		for (;*subpath; ++subpath) {
 			if (*subpath == GOOD_SLASH) {
-				mkdir(path.c_str(), 0700);
+				EnsureDir(path.c_str(), PL_PRIVATE);
 			}
 			path+= *subpath;
 		}

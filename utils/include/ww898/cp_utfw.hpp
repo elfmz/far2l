317793cc51846161b16cf7777ca8777569645c10
elfmz <fenix1905@tut.by>
2021-08-22 18:57:04 +0300

    fix freebsd

--- utils/include/ww898/cp_utfw.hpp
+++ utils/include/ww898/cp_utfw.hpp
@@ -33,7 +33,7 @@ namespace utf {
 using utfw = utf16;
 }}
 
-#elif defined(__linux__) || defined(__APPLE__)
+#elif defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__)
 
 #include <ww898/cp_utf32.hpp>
 

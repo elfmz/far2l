b648129c14995e38726beaee36476b428910412d
elfmz <fenix1905@tut.by>
2021-08-28 11:24:58 +0300

    fix build Mac and FreeBSD

--- far2l/farrtl.cpp
+++ far2l/farrtl.cpp
@@ -272,11 +272,35 @@ wchar_t * __cdecl xwcsncat(wchar_t * dest,const wchar_t * src, size_t DestSize)
 	return start;
 }
 
+#if defined(__APPLE__) || defined(__FreeBSD__)
+struct QSortExAdapterArg
+{
+	int (*__cdecl comp)(const void *, const void *, void *);
+	void *ctx;
+};
+
+static int QSortExAdapter(void *a, const void *left, const void *right)
+{
+	struct QSortExAdapterArg *aa = (struct QSortExAdapterArg *)a;
+	return aa->comp(left, right, aa->ctx);
+}
+
+void __cdecl far_qsortex(void *base, size_t num, size_t width,
+	int (*__cdecl comp)(const void *, const void *, void *), void *ctx)
+{
+	struct QSortExAdapterArg aa = { comp, ctx };
+	qsort_r(base, num, width, &aa, QSortExAdapter);
+}
+
+#else
+
 void __cdecl far_qsortex(void *base, size_t num, size_t width,
 	int (*__cdecl comp)(const void *, const void *, void *), void *ctx)
 {
 	qsort_r(base, num, width, comp, ctx);
 }
+
+#endif
 
 void __cdecl far_qsort(void *base, size_t num, size_t width,
     int (*__cdecl comp)(const void *, const void *)

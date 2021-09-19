813bba0e62dc6ed5ff4f624b0c510deb4fef6a79
elfmz <fenix1905@tut.by>
2021-09-19 20:10:36 +0300

    update copyrights nicknames translation; cosmetics

--- far2l/FARString.cpp
+++ far2l/FARString.cpp
@@ -420,7 +420,7 @@ FARString& FARString::Clear()
 static void FARStringFmt(FARString &str, bool append, const wchar_t *format, va_list argptr)
 {
 	const size_t StartSize = 0x200;		// 512 bytes ought to be enough for mosts
-	const size_t MaxSize = 0x1000000;	// 16 megs ought to be enough for everyone
+	const size_t MaxSize = 0x1000000;	// 16 megs ought to be enough for anybody
 	size_t Size;
 
 	for (Size = StartSize; Size <= MaxSize; Size<<= 1)

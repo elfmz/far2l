9b1e204004324419ef6462003fbe15b3ffb11a9c
elfmz <fenix1905@tut.by>
2022-11-08 11:48:35 +0300

    subfix #1392

--- WinPort/src/Backend/TTY/TTYOutput.cpp
+++ WinPort/src/Backend/TTY/TTYOutput.cpp
@@ -22,7 +22,7 @@
 
 #define ATTRIBUTES_AFFECTING_BACKGROUND \
 	(BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED | BACKGROUND_INTENSITY \
-	| BACKGROUND_TRUECOLOR | COMMON_LVB_REVERSE_VIDEO | COMMON_LVB_UNDERSCORE | COMMON_LVB_STRIKEOUT)
+	| BACKGROUND_TRUECOLOR | COMMON_LVB_UNDERSCORE | COMMON_LVB_STRIKEOUT)
 
 void TTYOutput::TrueColors::AppendSuffix(std::string &out, DWORD rgb)
 {
@@ -62,7 +62,9 @@ void TTYOutput::WriteUpdatedAttributes(DWORD64 attr, bool is_space)
 	}
 	if (is_space && (xa & ATTRIBUTES_AFFECTING_BACKGROUND) == 0) {
 		if ((attr & BACKGROUND_TRUECOLOR) == 0 || GET_RGB_BACK(xa) == 0) {
-			return;
+			if ( ((attr | _prev_attr) & COMMON_LVB_REVERSE_VIDEO) == 0) {
+				return;
+			}
 		}
 	}
 

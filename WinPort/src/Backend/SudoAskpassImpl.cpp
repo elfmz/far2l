34025c06042011f4a683fd719cbd87a5e5f2504a
elfmz <fenix1905@tut.by>
2022-11-19 23:57:58 +0300

    sudo: improve password typing experience (touch #1408)

--- WinPort/src/Backend/SudoAskpassImpl.cpp
+++ WinPort/src/Backend/SudoAskpassImpl.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <utils.h>
+#include <crc64.h>
 #include <SavedScreen.h>
 #include "Backend.h"
 #include "SudoAskpassImpl.h"
@@ -17,7 +18,10 @@ class SudoAskpassScreen
 		RES_OK,
 		RES_CANCEL
 	} _result = RES_PENDING;
+	bool _password_expected = false;
 	bool _need_repaint = false;
+
+	SMALL_RECT _rect{0}; // filled by Repaint()
 
 	void DispatchInputKey(const KEY_EVENT_RECORD &rec)
 	{
@@ -31,11 +35,14 @@ class SudoAskpassScreen
 			_result = RES_CANCEL;
 
 		} else if (rec.wVirtualKeyCode == VK_BACK) {
-			if (!_input.empty())
+			if (!_input.empty()) {
 				_input.resize(_input.size() - 1);
+				PaintPasswordPanno();
+			}
 
 		} else if (rec.uChar.UnicodeChar) {
 			_input+= rec.uChar.UnicodeChar;
+			PaintPasswordPanno();
 		}
 	}
 
@@ -77,44 +84,74 @@ class SudoAskpassScreen
 	void Repaint()
 	{
 		g_winport_con_out->GetSize(_width, _height);
-		SHORT w = std::max(_key_hint.size(), std::max(_title.size(), _text.size())) + 5, h = 5;
 
-		SHORT l = (SHORT(_width) - w) / 2, t = 0;//(_height - h) / 2;
-		if (l < 0) l = 0;
-//		if (t < 0) t = 0;
+		const SHORT w = std::max(_key_hint.size(), std::max(_title.size(), _text.size())) + 5;
 
-		COORD pos;
+		_rect.Left = std::max(0, (SHORT(_width) - w) / 2);
+		_rect.Right = _rect.Left + w - 1;
+		_rect.Top = 0;
+		_rect.Bottom = _rect.Top + 4;
 
-		for (SHORT y = t; y < t + h; ++y) {
-			for (SHORT x = l; x < l + w; ++x) {
-				pos = COORD {x, y};
-				if (x == l || y == t || x + 1 == l + w || y + 1 == t + h) {
+		for (SHORT y = _rect.Top; y <= _rect.Bottom; ++y) {
+			for (SHORT x = _rect.Left; x <= _rect.Right; ++x) {
+				COORD pos{x, y};
+				if (x == _rect.Left || x == _rect.Right || y == _rect.Top || y == _rect.Bottom) {
 					g_winport_con_out->FillAttributeAt(FOREGROUND_RED | BACKGROUND_RED, 1, pos);
 				} else {
 					g_winport_con_out->FillAttributeAt(
 						FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE, 1, pos);
 				}
-				pos = COORD {x, y};
+				pos = COORD{x, y};
 				g_winport_con_out->FillCharacterAt(' ', 1, pos);
 			}
 		}
 
 		g_winport_con_out->SetAttributes(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-		WriteCentered(_title, t + 1);
-		WriteCentered(_key_hint, t + 3);
+		WriteCentered(_title, _rect.Top + 1);
+		WriteCentered(_key_hint, _rect.Top + 3);
 
 		g_winport_con_out->SetAttributes(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-		WriteCentered(_text, t + 2);
+		WriteCentered(_text, _rect.Top + 2);
 //		COORD pos = {};
 //		g_winport_con_out->FillAttributeAt(FOREGROUND_GREEN, _width * _height, pos);
+		PaintPasswordPanno();
+	}
+
+	void PaintPasswordPanno()
+	{
+		if (!_password_expected) {
+			return;
+		}
+
+		const wchar_t glyphs[] = {L'■', L'▲', L'●'};
+
+		uint64_t hash64 = _input.empty() ? 0 : crc64(0x1215814a,
+			(const unsigned char *)_input.c_str(), _input.size() * sizeof(*_input.c_str()));
+
+		for (SHORT i = -4; i < 4; ++i, hash64>>= 8) {
+			CHAR_INFO ci{};
+			ci.Attributes = BACKGROUND_RED;
+			switch ((hash64 & 0xf) % 4) {
+				case 0: ci.Attributes|= FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY; break;
+				case 1: ci.Attributes|= FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY; break;
+				case 2: ci.Attributes|= FOREGROUND_GREEN | FOREGROUND_INTENSITY; break;
+				case 3: ci.Attributes|= FOREGROUND_BLUE | FOREGROUND_INTENSITY; break;
+			}
+
+			CI_SET_WCHAR(ci, glyphs[((hash64 >> 4) & 0xf) % ARRAYSIZE(glyphs)]);
+
+			COORD pos{SHORT(SHORT(_width / 2) + i), _rect.Bottom};
+			g_winport_con_out->Write(ci, pos);
+		}
 	}
 
 public:
-	SudoAskpassScreen(const std::string &title, const std::string &text) :
+	SudoAskpassScreen(const std::string &title, const std::string &text, bool password_expected) :
 		_cip(g_winport_con_in),
 		_title(title),
 		_text(text),
-		_key_hint("Confirm by <Enter> Cancel by <Esc>")
+		_key_hint("Confirm by <Enter> Cancel by <Esc>"),
+		_password_expected(password_expected)
 	{
 		_ir_resized.EventType = NOOP_EVENT;
 		Repaint();
@@ -154,7 +191,7 @@ public:
 
 bool SudoAskpassImpl::OnSudoAskPassword(const std::string &title, const std::string &text, std::string &password)
 {
-	SudoAskpassScreen sas(title, text);
+	SudoAskpassScreen sas(title, text, true);
 	if (!sas.Loop())
 		return false;
 
@@ -164,6 +201,6 @@ bool SudoAskpassImpl::OnSudoAskPassword(const std::string &title, const std::str
 
 bool SudoAskpassImpl::OnSudoConfirm(const std::string &title, const std::string &text)
 {
-	SudoAskpassScreen sas(title, text);
+	SudoAskpassScreen sas(title, text, false);
 	return sas.Loop();
 }

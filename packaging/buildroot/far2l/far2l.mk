c393db9ddf411f91fcae9b63a3c83e4b90ec71b1
elfmz <fenix1905@tut.by>
2022-11-29 02:03:27 +0300

    add buildroot package

---
+++ packaging/buildroot/far2l/far2l.mk
@@ -0,0 +1,55 @@
+################################################################################
+#
+# unzip
+#
+################################################################################
+
+FAR2L_VERSION = master
+FAR2L_SITE = $(call github,elfmz,far2l,$(FAR2L_VERSION))
+FAR2L_LICENSE = GPL-2
+FAR2L_LICENSE_FILES = LICENSE.txt
+FAR2L_INSTALL_STAGING = YES
+
+FAR2L_DEPENDENCIES = \
+	fmt \
+	spdlog \
+	libarchive \
+	xerces \
+	libnfs \
+	libssh
+
+# broken in buildroot for now	neon 
+
+ifeq ($(BR2_PACKAGE_FAR2L_COLORER),y)
+FAR2L_DEPENDENCIES += spdlog xerces
+endif
+
+ifeq ($(BR2_PACKAGE_FAR2L_NETROCKS),y)
+FAR2L_DEPENDENCIES += libssh libnfs neon libopenssl
+endif
+
+ifeq ($(BR2_PACKAGE_FAR2L_MULTIARC),y)
+FAR2L_DEPENDENCIES += pcre2 libarchive
+endif
+
+FAR2L_CONF_OPTS = \
+	-DTTYX=$(if $(BR2_PACKAGE_FAR2L_TTYXI),ON,OFF) \
+	-DTTYXi=$(if $(BR2_PACKAGE_FAR2L_TTYXI),ON,OFF) \
+	-DALIGN=$(if $(BR2_PACKAGE_FAR2L_ALIGN),ON,OFF) \
+	-DAUTOWRAP=$(if $(BR2_PACKAGE_FAR2L_AUTOWRAP),ON,OFF) \
+	-DCALC=$(if $(BR2_PACKAGE_FAR2L_CALC),ON,OFF) \
+	-DCOLORER=$(if $(BR2_PACKAGE_FAR2L_COLORER),ON,OFF) \
+	-DCOMPARE=$(if $(BR2_PACKAGE_FAR2L_COMPARE),ON,OFF) \
+	-DDRAWLINE=$(if $(BR2_PACKAGE_FAR2L_DRAWLINE),ON,OFF) \
+	-DEDITCASE=$(if $(BR2_PACKAGE_FAR2L_EDITCASE),ON,OFF) \
+	-DEDITORCOMP=$(if $(BR2_PACKAGE_FAR2L_EDITORCOMP),ON,OFF) \
+	-DFILECASE=$(if $(BR2_PACKAGE_FAR2L_FILECASE),ON,OFF) \
+	-DINCSRCH=$(if $(BR2_PACKAGE_FAR2L_INCSRCH),ON,OFF) \
+	-DINSIDE=$(if $(BR2_PACKAGE_FAR2L_INSIDE),ON,OFF) \
+	-DMULTIARC=$(if $(BR2_PACKAGE_FAR2L_MULTIARC),ON,OFF) \
+	-DNETROCKS=$(if $(BR2_PACKAGE_FAR2L_NETROCKS),ON,OFF) \
+	-DSIMPLEINDENT=$(if $(BR2_PACKAGE_FAR2L_SIMPLEINDENT),ON,OFF) \
+	-DTMPPANEL=$(if $(BR2_PACKAGE_FAR2L_TMPPANEL),ON,OFF) \
+	-DUSEUCD=OFF -DUSEWX=OFF -DNR_WEBDAV=OFF
+
+$(eval $(cmake-package))

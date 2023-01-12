3abe21167f3b2f8096a5b497131bbb22fce7cf6a
elfmz <fenix1905@tut.by>
2023-01-12 23:32:02 +0300

    rename atoul/htoul to natoul/nhtoul to avoid conflicts

--- utils/include/IntStrConv.h
+++ utils/include/IntStrConv.h
@@ -10,8 +10,8 @@ int64_t _wtoi64(const wchar_t *w);
 char * _i64toa(int64_t i, char *a, int radix);
 wchar_t * _i64tow(int64_t i, wchar_t *w, int radix);
 wchar_t * _itow(int i, wchar_t *w, int radix);
-unsigned long htoul(const char *str, size_t maxlen);
-unsigned long atoul(const char *str, size_t maxlen);
+unsigned long nhtoul(const char *str, size_t maxlen);
+unsigned long natoul(const char *str, size_t maxlen);
 
 #ifdef __cplusplus
 }

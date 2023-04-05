d11336e35d2b44bd8eedac32ea9cc0e9f9a9ec9c
Ivan Sorokin <unxed@mail.ru>
2023-04-05 09:49:24 +0200

    added missing numpad scan codes; disable compact input for right shift as it treats it as left shift

--- WinPort/WinCompat.h
+++ WinPort/WinCompat.h
@@ -1517,3 +1517,8 @@ typedef VOID (*PCONSOLE_SCROLL_CALLBACK)(PVOID pContext, unsigned int Width, CHA
 #endif
 
 #define CONSOLE_FKEYS_COUNT 12 // array count for SetConsoleFKeyTitles
+
+// Virtual Scan Code for right Shift key
+// As described here
+// https://docs.vmware.com/en/VMware-Workstation-Player-for-Windows/17.0/com.vmware.player.win.using.doc/GUID-D2C43B86-32EF-44EA-A2ED-D890483D70BD.html
+#define RIGHT_SHIFT_VSC 54

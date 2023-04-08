78d4dd5f37842ff0caba6f059df919132338c791
Ivan Sorokin <unxed@mail.ru>
2023-04-08 04:03:50 +0200

    added support for kovidgoyal's kitty keyboard protocol as described here https://sw.kovidgoyal.net/kitty/keyboard-protocol/

--- WinPort/src/Backend/TTY/TTYOutput.cpp
+++ WinPort/src/Backend/TTY/TTYOutput.cpp
@@ -163,6 +163,7 @@ TTYOutput::TTYOutput(int out, bool far2l_tty)
 
 	Format(ESC "7" ESC "[?47h" ESC "[?1049h" ESC "[?2004h");
 	Format(ESC "[?9001h"); // win32-input-mode on
+	Format(ESC "[=15;1u"); // kovidgoyal's kitty mode on
 	ChangeKeypad(true);
 	ChangeMouse(true);
 
@@ -188,6 +189,7 @@ TTYOutput::~TTYOutput()
 		}
 		Format(ESC "[0m" ESC "[?1049l" ESC "[?47l" ESC "8" ESC "[?2004l" "\r\n");
 		Format(ESC "[?9001l"); // win32-input-mode off
+		Format(ESC "[=0;1u"); // kovidgoyal's kitty mode off
 		TTYBasePalette def_palette;
 		ChangePalette(def_palette);
 		Flush();

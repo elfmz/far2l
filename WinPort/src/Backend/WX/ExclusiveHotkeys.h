cf2fe9c222ec692e36dfa44a9807b13045300a11
Dmitry Pankratov <dmitry@pankratov.net>
2023-04-08 22:40:53 +0200

    Compilation fixes with recent compiler versions (GCC 13, clang 16).

--- WinPort/src/Backend/WX/ExclusiveHotkeys.h
+++ WinPort/src/Backend/WX/ExclusiveHotkeys.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <wx/wx.h>
-
+#include <cstdint>
 
 class ExclusiveHotkeys
 {
--- inside/src/elf/Dumper.h
+++ inside/src/elf/Dumper.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <set>
+#include <cstdint>
 
 namespace Root
 {

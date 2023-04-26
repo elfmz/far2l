76e5e986d5c409a7a54162ce62e851856b28e287
m32 <grzegorz.makarewicz@gmail.com>
2023-04-26 12:23:54 +0200

    without api limiting

--- python/src/python.cpp
+++ python/src/python.cpp
@@ -378,8 +378,6 @@ XPORT(int, MakeDirectory)(HANDLE hPlugin,const wchar_t **Name,int OpMode) {
     PYTHON_INT(0)
     return result;
 }
-#if 0
-// in far2l only one plugin is allowed at time
 XPORT(HANDLE, OpenFilePlugin)(const wchar_t *Name,const unsigned char *Data,int DataSize,int OpMode) {
     PYTHON_LOG("OpMode=%d Name='%ls'\n", OpMode, Name);
     HANDLE result = INVALID_HANDLE_VALUE;
@@ -387,7 +385,6 @@ XPORT(HANDLE, OpenFilePlugin)(const wchar_t *Name,const unsigned char *Data,int 
     PYTHON_HANDLE(INVALID_HANDLE_VALUE)
     return result;
 }
-#endif
 XPORT(int, ProcessDialogEvent)(int Event,void *Param) {
     PYTHON_LOG("Event=%d\n", Event);
     int result = 0;

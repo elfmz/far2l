c7a52c89b06361d0382aa18b52463c316c668138
elfmz <fenix1905@tut.by>
2023-04-27 22:41:09 +0300

    refactor

---
+++ far2l/bootstrap/scripts/far2l.pm
@@ -0,0 +1,38 @@
+package far2l;
+
+sub UpdateFromTmp
+{
+	my ($tmp, $path) = (@_);
+	if (!AreSameFiles($tmp, $path)) {
+		print "Updating $path\n";
+		system("cp -f $tmp $path");
+	}
+	unlink("$tmp");
+}
+
+sub AreSameFiles
+{
+	my ($path1, $path2) = (@_);
+	my ($f1, $f2);
+	return undef if !open ($f1, '<', $path1);
+	if (!open($f2, '<', $path2)) {
+		close($f1);
+		return undef;
+	}
+	my $out = 1;
+	for (;;) {
+		my $s1 = <$f1>;
+		my $s2 = <$f2>;
+		last if !defined($s1) && !defined($s2);
+		if (!defined($s1) || !defined($s2) || $s1 ne $s2) {
+			$out = undef;
+			last;
+		}
+	}
+
+	close($f1);
+	close($f2);
+	return $out;
+}
+
+1;
--- far2l/bootstrap/scripts/farver.pl
+++ far2l/bootstrap/scripts/farver.pl
@@ -1,5 +1,6 @@
-#!/usr/bin/env perl
+#!/usr/bin/env -S perl -I.
 use strict;
+use far2l;
 
 my $path = shift;
 my $arch = shift;
@@ -21,34 +22,4 @@ print $f "const char *FAR_BUILD __attribute__((used)) = \"$major.$minor.$patch\"
 print $f "const char *Copyright __attribute__((used)) = \"FAR2L, version $major.$minor.$patch $arch\\nCopyright (C) 1996-2000 Eugene Roshal, Copyright (C) 2000-2016 Far Group, Copyright (C) 2016-$year Far People\";\n";
 close $f;
 
-if (!AreSameFiles($tmp, $path)) {
-	print "Updating $path\n";
-	system("cp -f $tmp $path") 
-}
-unlink("$tmp");
-
-## TODO: Move to dedicated .pm file
-sub AreSameFiles
-{
-	my ($path1, $path2) = (@_);
-	my ($f1, $f2);
-	return undef if !open ($f1, '<', $path1);
-	if (!open($f2, '<', $path2)) {
-		close($f1);
-		return undef;
-	}
-	my $out = 1;
-	for (;;) {
-		my $s1 = <$f1>;
-		my $s2 = <$f2>;
-		last if !defined($s1) && !defined($s2);
-		if (!defined($s1) || !defined($s2) || $s1 ne $s2) {
-			$out = undef;
-			last;
-		}
-	}
-
-	close($f1);
-	close($f2);
-	return $out;
-}
+far2l::UpdateFromTmp($tmp, $path);

1e3b707a406a9143a7315a5e3c8595098c82abaa
elfmz <fenix1905@tut.by>
2023-07-04 10:17:36 +0300

    NetRocks: refactor times/modes cache and apply it also to SCP (touch #1708)

--- NetRocks/src/Protocol/FileStatsOverride.h
+++ NetRocks/src/Protocol/FileStatsOverride.h
@@ -1,8 +1,10 @@
 #pragma once
-#include <map>
-#include <string>
 #include <time.h>
 #include <sys/stat.h>
+#include <map>
+#include <string>
+#include <memory>
+#include "Protocol.h"
 
 class FileStatsOverride
 {
@@ -14,13 +16,31 @@ public:
 		mode_t mode{};
 	};
 
+	bool NonEmpty() const { return !_path2ovrst.empty(); }
 	void Cleanup(const std::string &path);
 	void Rename(std::string old_path, std::string new_path);
 	void OverrideTimes(const std::string &path, const timespec &access_time, const timespec &modification_time);
 	void OverrideMode(const std::string &path, mode_t mode);
-	const OverridenStats *Lookup(const std::string &path);
-	const OverridenStats *Lookup(const std::string &path, const std::string &name);
+	const OverridenStats *Lookup(const std::string &path) const;
+	const OverridenStats *Lookup(const std::string &path, const std::string &name) const;
 
 private:
 	std::map<std::string, OverridenStats> _path2ovrst;
 };
+
+
+class DirectoryEnumerWithFileStatsOverride : public IDirectoryEnumer
+{
+	const FileStatsOverride &_file_stats_override;
+	std::shared_ptr<IDirectoryEnumer> _enumer;
+	std::string _path;
+
+public:
+	DirectoryEnumerWithFileStatsOverride(const FileStatsOverride &file_stats_override,
+		std::shared_ptr<IDirectoryEnumer> enumer, const std::string &path);
+	virtual ~DirectoryEnumerWithFileStatsOverride();
+	virtual bool Enum(std::string &name, std::string &owner, std::string &group, FileInformation &file_info);
+};
+
+void GetInformationWithFileStatsOverride(const FileStatsOverride &file_stats_override, FileInformation &file_info, const std::string &path);
+void GetModeWithFileStatsOverride(const FileStatsOverride &file_stats_override, mode_t &mode, const std::string &path);

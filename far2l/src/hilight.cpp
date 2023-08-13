d8451d912982103af384da051306e41846fc9e21
elfmz <fenix1905@tut.by>
2023-08-14 01:16:13 +0300

    fix wrong transparent check when using RGB colors (fix #1797)

--- far2l/src/hilight.cpp
+++ far2l/src/hilight.cpp
@@ -296,12 +296,12 @@ static void ApplyColors(HighlightDataColor *DestColors, HighlightDataColor *SrcC
 			// то унаследуем их в Dest.
 			if (!(SrcColors->Color[j][i] & 0xF000)) {
 				DestColors->Color[j][i] =
-						(DestColors->Color[j][i] & 0xFFFFFF0000000F0F) | (SrcColors->Color[j][i] & 0x000000FFFFFFF0F0);
+						(DestColors->Color[j][i] & 0x000000FFFFFF0F0F) | (SrcColors->Color[j][i] & 0xFFFFFF000000F0F0);
 			}
 
 			if (!(SrcColors->Color[j][i] & 0x0F00)) {
 				DestColors->Color[j][i] =
-						(DestColors->Color[j][i] & 0x000000FFFFFFF0F0) | (SrcColors->Color[j][i] & 0xFFFFFF0000000F0F);
+						(DestColors->Color[j][i] & 0xFFFFFF000000F0F0) | (SrcColors->Color[j][i] & 0x000000FFFFFF0F0F);
 			}
 		}
 	}

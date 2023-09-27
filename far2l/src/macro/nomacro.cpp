aec313d151999cefe30f85a82af130575e1d8702
Ivan Sorokin <unxed@mail.ru>
2023-09-27 14:32:50 +0200

    leave only really used methods

--- far2l/src/macro/nomacro.cpp
+++ far2l/src/macro/nomacro.cpp
@@ -12,53 +12,32 @@ nomacro.hpp
 #include "macroopcode.hpp"
 #include "nomacro.hpp"
 
+#include "nomacro.hpp"
+
 KeyMacro::KeyMacro() {}
 KeyMacro::~KeyMacro() {}
 uint32_t KeyMacro::ProcessKey(uint32_t Key) { return 0; }
 int KeyMacro::GetKey() { return 0; }
 int KeyMacro::PeekKey() { return 0; }
-bool KeyMacro::IsOpCode(DWORD p) { return false; }
 bool KeyMacro::CheckWaitKeyFunc() { return false; }
-int KeyMacro::PushState(bool CopyLocalVars) { return 0; }
-int KeyMacro::PopState() { return 0; }
-int KeyMacro::GetLevelState() { return 0; }
-int KeyMacro::IsRecording() { return 0; }
-int KeyMacro::IsExecuting() { return 0; }
 int KeyMacro::IsExecutingLastKey() { return 0; }
-int KeyMacro::IsDsableOutput() { return 0; }
-void KeyMacro::SetMode(int Mode) {}
-int KeyMacro::GetMode() { return 0; }
-void KeyMacro::DropProcess() {}
 void KeyMacro::RunStartMacro() {}
 int KeyMacro::PostNewMacro(const wchar_t *PlainText, DWORD Flags, DWORD AKey, BOOL onlyCheck) { return 0; }
 int KeyMacro::PostNewMacro(MacroRecord *MRec, BOOL NeedAddSendFlag, BOOL IsPluginSend) { return 0; }
 int KeyMacro::LoadMacros(BOOL InitedRAM, BOOL LoadAll) { return 0; }
 void KeyMacro::SaveMacros(BOOL AllSaved) {}
-int KeyMacro::GetStartIndex(int Mode) { return 0; }
 int KeyMacro::GetIndex(uint32_t Key, int Mode, bool UseCommon) { return 0; }
-int KeyMacro::GetRecordSize(int Key, int Mode) { return 0; }
-bool KeyMacro::GetPlainText(FARString &Dest) { return false; }
-int KeyMacro::GetPlainTextSize() { return 0; }
-void KeyMacro::SetRedrawEditor(int Sets) {}
-void KeyMacro::RestartAutoMacro(int Mode) {}
-int KeyMacro::GetCurRecord(MacroRecord *RBuf, int *KeyPos) { return 0; }
 BOOL KeyMacro::CheckCurMacroFlags(DWORD Flags) { return FALSE; }
+int KeyMacro::GetCurRecord(struct MacroRecord *RBuf, int *KeyPos) { return 0; }
 const wchar_t *KeyMacro::GetSubKey(int Mode) { return L""; }
 int KeyMacro::GetSubKey(const wchar_t *Mode) { return 0; }
 int KeyMacro::GetMacroKeyInfo(bool FromReg, int Mode, int Pos, FARString &strKeyName, FARString &strDescription)
 	{ return 0; }
-wchar_t *KeyMacro::MkTextSequence(DWORD *Buffer, int BufferSize, const wchar_t *Src) { return nullptr; }
 int KeyMacro::ParseMacroString(MacroRecord *CurMacro, const wchar_t *BufPtr, BOOL onlyCheck) { return 0; }
 BOOL KeyMacro::GetMacroParseError(DWORD *ErrCode, COORD *ErrPos, FARString *ErrSrc) { return FALSE; }
 BOOL KeyMacro::GetMacroParseError(FARString *Err1, FARString *Err2, FARString *Err3, FARString *Err4)
 	{ return FALSE; }
 void KeyMacro::SetMacroConst(const wchar_t *ConstName, const TVar Value) {}
-DWORD KeyMacro::GetNewOpCode() { return 0; }
-size_t KeyMacro::GetCountMacroFunction() { return 0; }
-const TMacroFunction *KeyMacro::GetMacroFunction(size_t Index) { return nullptr; }
-void KeyMacro::RegisterMacroIntFunction() {}
-TMacroFunction *KeyMacro::RegisterMacroFunction(const TMacroFunction *tmfunc) { return nullptr; }
-bool KeyMacro::UnregMacroFunction(size_t Index) { return false; }
 
 ChangeMacroMode::ChangeMacroMode(int NewMode) {}
 ChangeMacroMode::~ChangeMacroMode() {}
--- far2l/src/macro/nomacro.hpp
+++ far2l/src/macro/nomacro.hpp
@@ -8,191 +8,33 @@ nomacro.hpp
 
 */
 
-class Panel;
-
-struct TMacroFunction;
-typedef bool (*INTMACROFUNC)(const TMacroFunction *);
-
-enum INTMF_FLAGS
-{
-    IMFF_UNLOCKSCREEN = 0x00000001,
-    IMFF_DISABLEINTINPUT = 0x00000002,
-};
-
-struct TMacroFunction
-{
-    const wchar_t *Name;
-    int nParam;
-    int oParam;
-    TMacroOpCode Code;
-    const wchar_t *fnGUID;
-
-    int BufferSize;
-    DWORD *Buffer;
-    const wchar_t *Syntax;
-
-    DWORD IntFlags;
-    INTMACROFUNC Func;
-};
-
-struct MacroRecord
-{
-    DWORD Flags;
-    uint32_t Key;
-    int BufferSize;
-    DWORD *Buffer;
-    wchar_t *Src;
-    wchar_t *Description;
-    DWORD Reserved[2];
-};
-
-#define STACKLEVEL 32
-
-struct MacroState
-{
-    int KeyProcess;
-    int Executing;
-    int MacroPC;
-    int ExecLIBPos;
-    int MacroWORKCount;
-    bool UseInternalClipboard;
-    struct MacroRecord *MacroWORK;
-    INPUT_RECORD cRec;
-
-    bool AllocVarTable;
-    TVarTable *locVarTable;
-
-    void Init(TVarTable *tbl);
-};
-
-struct MacroPanelSelect
-{
-    int Action;
-    DWORD ActionFlags;
-    int Mode;
-    int64_t Index;
-    TVar *Item;
-};
-
-enum MACROMODEAREA
-{
-    MACRO_FUNCS = -3,
-    MACRO_CONSTS = -2,
-    MACRO_VARS = -1,
-
-    MACRO_OTHER = 0,
-    MACRO_SHELL = 1,
-    MACRO_VIEWER = 2,
-    MACRO_EDITOR = 3,
-    MACRO_DIALOG = 4,
-    MACRO_SEARCH = 5,
-    MACRO_DISKS = 6,
-    MACRO_MAINMENU = 7,
-    MACRO_MENU = 8,
-    MACRO_HELP = 9,
-    MACRO_INFOPANEL = 10,
-    MACRO_QVIEWPANEL = 11,
-    MACRO_TREEPANEL = 12,
-    MACRO_FINDFOLDER = 13,
-    MACRO_USERMENU = 14,
-    MACRO_AUTOCOMPLETION = 15,
-
-    MACRO_COMMON,
-    MACRO_LAST
-};
-
-enum MACROFLAGS_MFLAGS
-{
-    MFLAGS_MODEMASK = 0x000000FF,
-
-    MFLAGS_DISABLEOUTPUT = 0x00000100,
-    MFLAGS_NOSENDKEYSTOPLUGINS = 0x00000200,
-    MFLAGS_RUNAFTERFARSTARTED = 0x00000400,
-    MFLAGS_RUNAFTERFARSTART = 0x00000800,
-    MFLAGS_EMPTYCOMMANDLINE = 0x00001000,
-    MFLAGS_NOTEMPTYCOMMANDLINE = 0x00002000,
-    MFLAGS_EDITSELECTION = 0x00004000,
-    MFLAGS_EDITNOSELECTION = 0x00008000,
-    MFLAGS_SELECTION = 0x00010000,
-    MFLAGS_PSELECTION = 0x00020000,
-    MFLAGS_NOSELECTION = 0x00040000,
-    MFLAGS_PNOSELECTION = 0x00080000,
-    MFLAGS_NOFILEPANELS = 0x00100000,
-    MFLAGS_PNOFILEPANELS = 0x00200000,
-    MFLAGS_NOPLUGINPANELS = 0x00400000,
-    MFLAGS_PNOPLUGINPANELS = 0x00800000,
-    MFLAGS_NOFOLDERS = 0x01000000,
-    MFLAGS_PNOFOLDERS = 0x02000000,
-    MFLAGS_NOFILES = 0x04000000,
-    MFLAGS_PNOFILES = 0x08000000,
-    MFLAGS_NEEDSAVEMACRO = 0x40000000,
-    MFLAGS_DISABLEMACRO = 0x80000000,
-};
-
 class KeyMacro
 {
-public:
-    KeyMacro();
-    ~KeyMacro();
 
 public:
-    uint32_t ProcessKey(uint32_t Key);
+	KeyMacro();
+	~KeyMacro();
+
+	uint32_t ProcessKey(uint32_t Key);
     int GetKey();
-    int PeekKey();
-    bool IsOpCode(DWORD p);
-    bool CheckWaitKeyFunc();
-
-    int PushState(bool CopyLocalVars = FALSE);
-    int PopState();
-    int GetLevelState();
-
-    int IsRecording();
-    int IsExecuting();
+	int PeekKey();
+	bool CheckWaitKeyFunc();
     int IsExecutingLastKey();
-    int IsDsableOutput();
-    void SetMode(int Mode);
-    int GetMode();
-
-    void DropProcess();
-
     void RunStartMacro();
-
     int PostNewMacro(const wchar_t *PlainText, DWORD Flags = 0, DWORD AKey = 0, BOOL onlyCheck = FALSE);
     int PostNewMacro(struct MacroRecord *MRec, BOOL NeedAddSendFlag = 0, BOOL IsPluginSend = FALSE);
-
     int LoadMacros(BOOL InitedRAM = TRUE, BOOL LoadAll = TRUE);
-    void SaveMacros(BOOL AllSaved = TRUE);
-
-    int GetStartIndex(int Mode);
-    int GetIndex(uint32_t Key, int Mode, bool UseCommon = true);
-    int GetRecordSize(int Key, int Mode);
-
-    bool GetPlainText(FARString &Dest);
-    int GetPlainTextSize();
-
-    void SetRedrawEditor(int Sets);
-
-    void RestartAutoMacro(int Mode);
-
-    int GetCurRecord(struct MacroRecord *RBuf = nullptr, int *KeyPos = nullptr);
+	void SaveMacros(BOOL AllSaved = TRUE);
+	int GetIndex(uint32_t Key, int Mode, bool UseCommon = true);
     BOOL CheckCurMacroFlags(DWORD Flags);
-
+	int GetCurRecord(struct MacroRecord *RBuf = nullptr, int *KeyPos = nullptr);
     static const wchar_t *GetSubKey(int Mode);
     static int GetSubKey(const wchar_t *Mode);
     static int GetMacroKeyInfo(bool FromReg, int Mode, int Pos, FARString &strKeyName, FARString &strDescription);
-    static wchar_t *MkTextSequence(DWORD *Buffer, int BufferSize, const wchar_t *Src = nullptr);
-    int ParseMacroString(struct MacroRecord *CurMacro, const wchar_t *BufPtr, BOOL onlyCheck = FALSE);
+	int ParseMacroString(struct MacroRecord *CurMacro, const wchar_t *BufPtr, BOOL onlyCheck = FALSE);
     BOOL GetMacroParseError(DWORD *ErrCode, COORD *ErrPos, FARString *ErrSrc);
     BOOL GetMacroParseError(FARString *Err1, FARString *Err2, FARString *Err3, FARString *Err4);
-
-    static void SetMacroConst(const wchar_t *ConstName, const TVar Value);
-    static DWORD GetNewOpCode();
-
-    static size_t GetCountMacroFunction();
-    static const TMacroFunction *GetMacroFunction(size_t Index);
-    static void RegisterMacroIntFunction();
-    static TMacroFunction *RegisterMacroFunction(const TMacroFunction *tmfunc);
-    static bool UnregMacroFunction(size_t Index);
+	static void SetMacroConst(const wchar_t *ConstName, const TVar Value);
 };
 
 class ChangeMacroMode

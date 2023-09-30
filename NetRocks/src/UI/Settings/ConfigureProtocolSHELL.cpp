cc970c0c43fa5cd1750273a61b1944751cb9b643
elfmz <fenix1905@tut.by>
2023-10-01 01:53:05 +0300

    fix path to ini file

--- NetRocks/src/UI/Settings/ConfigureProtocolSHELL.cpp
+++ NetRocks/src/UI/Settings/ConfigureProtocolSHELL.cpp
@@ -99,6 +99,7 @@ public:
 		_ways_ini = StrWide2MB(G.plugin_path);
 		CutToSlash(_ways_ini, true);
 		_ways_ini+= "SHELL/ways.ini";
+		TranslateInstallPath_Lib2Share(_ways_ini);
 
 		InitializeWays();
 

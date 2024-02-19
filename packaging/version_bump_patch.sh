54781ca16bbab7db2aae832d56b7242469526c78
elfmz <fenix1905@tut.by>
2024-02-19 22:31:08 +0300

    make new version controllable from command line

--- packaging/version_bump_patch.sh
+++ packaging/version_bump_patch.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-NEW_VERSION=$(perl -F'\.' -lane 'print $F[0].".".$F[1].".".($F[2]+1)' ./version)
+NEW_VERSION=$1
+if [ $NEW_VERSION = '' ]; then
+	NEW_VERSION=$(perl -F'\.' -lane 'print $F[0].".".$F[1].".".($F[2]+1)' ./version)
+fi
 echo NEW_VERSION=$NEW_VERSION
 echo $NEW_VERSION > ./version
 git add ./version

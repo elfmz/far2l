ba6e368642510c373565c68d78c060a2e5f9b6f4
elfmz <fenix1905@tut.by>
2024-03-03 22:43:46 +0300

    NR: make confirm new server identity dialog wider (fix #2030)

--- NetRocks/src/UI/Activities/ConfirmNewServerIdentity.cpp
+++ NetRocks/src/UI/Activities/ConfirmNewServerIdentity.cpp
@@ -5,7 +5,7 @@
 
 
 /*                                               
-345                                            50
+345                                            60
  ======== Confirm new server identify =========
 | Site:          [                           ] |
 | New identity:                                |
@@ -21,17 +21,17 @@ ConfirmNewServerIdentity::ConfirmNewServerIdentity(const std::string &site, cons
 	_di.SetBoxTitleItem(MNewServerIdentityTitle);
 
 	_di.SetLine(2);
-	_di.AddAtLine(DI_TEXT, 5,19, 0, MNewServerIdentitySite);
-	_di.AddAtLine(DI_TEXT, 20,48, 0, site.c_str());
+	_di.AddAtLine(DI_TEXT, 5,29, 0, MNewServerIdentitySite);
+	_di.AddAtLine(DI_TEXT, 30,58, 0, site.c_str());
 
 	_di.NextLine();
-	_di.AddAtLine(DI_TEXT, 5,48, 0, MNewServerIdentityText);
+	_di.AddAtLine(DI_TEXT, 5,58, 0, MNewServerIdentityText);
 
 	_di.NextLine();
-	_di.AddAtLine(DI_TEXT, 6,48, 0, identity.c_str());
+	_di.AddAtLine(DI_TEXT, 6,58, 0, identity.c_str());
 
 	_di.NextLine();
-	_di.AddAtLine(DI_TEXT, 4,49, DIF_BOXCOLOR | DIF_SEPARATOR);
+	_di.AddAtLine(DI_TEXT, 4,59, DIF_BOXCOLOR | DIF_SEPARATOR);
 
 	_di.NextLine();
 	_i_allow_once = _di.AddAtLine(DI_BUTTON, 6,15, DIF_CENTERGROUP, MNewServerIdentityAllowOnce);

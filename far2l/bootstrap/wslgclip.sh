6389d740d90c62d67f235f8f258ea2cdf2a2e499
Ivan Sorokin <unxed@mail.ru>
2024-03-25 19:55:57 +0000

    fix clipboard on wslg

---
+++ far2l/bootstrap/wslgclip.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# workaround for https://github.com/elfmz/far2l/issues/1658
+
+script_path=$(dirname "$(readlink -f "$0")")
+
+case "$1" in
+get)
+    if command -v cscript.exe >/dev/null 2>&1; then
+        cscript.exe //Nologo \\\\wsl.localhost\\"$WSL_DISTRO_NAME""$script_path"\\wslgclip.vbs
+    else
+        powershell.exe -Command Get-Clipboard
+    fi
+
+;;
+set)
+    CONTENT=$(cat)
+    echo "$CONTENT" | clip.exe
+    echo "$CONTENT"
+;;
+"")
+    (far2l --clipboard=$(readlink -f $0) >/dev/null 2>&1 &)
+;;
+esac
+

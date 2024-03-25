660d873bb15032fbf4e34fe20158225d80e3138f
Ivan Sorokin <unxed@mail.ru>
2024-03-25 21:38:45 +0000

    debugx

--- WinPort/src/Backend/WinPortMain.cpp
+++ WinPort/src/Backend/WinPortMain.cpp
@@ -453,15 +453,24 @@ extern "C" int WinPortMain(const char *full_exe_path, int argc, char **argv, int
 
 				    char buf[PATH_MAX];
 				    ssize_t len = readlink("/proc/self/exe", buf, sizeof(buf)-1);
+					fprintf(stderr, "*0 %i\n", len);
 				    if (len != -1) {
 				        buf[len] = '\0';
+
 				        std::string path(buf);
+
+						fprintf(stderr, "*1 %s\n", path.c_str());
 
 						if (TranslateInstallPath_Bin2Share(path)) {
 							path += APP_BASENAME "/";
 						}
 
+						fprintf(stderr, "*2 %s\n", path.c_str());
+
 						path += "/wslgclip.sh";
+
+						fprintf(stderr, "*3 %s\n", path.c_str());
+
 						arg_opts.ext_clipboard = path;
 						ext_clipboard_backend_setter.Set<ExtClipboardBackend>(arg_opts.ext_clipboard.c_str());
 						wsl_clipboard_workaround = true;

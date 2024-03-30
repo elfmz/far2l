d3de7152f7ecb8b6f5fa9e495bdb1f41fd84113f
atolismesh <130924690+atolismesh@users.noreply.github.com>
2024-03-30 17:47:21 +0300

    Fix build under Ubuntu 18.04 - update WinPortMain.cpp
    Fix build under Ubuntu 18.04 - remove  unnecessary includes from WinPortMain.cpp

--- WinPort/src/Backend/WinPortMain.cpp
+++ WinPort/src/Backend/WinPortMain.cpp
@@ -34,10 +34,6 @@
 #include "../sudo/sudo_askpass_ipc.h"
 #include "SudoAskpassImpl.h"
 
-#include <memory>
-#include <iostream>
-#include <filesystem>
-
 IConsoleOutput *g_winport_con_out = nullptr;
 IConsoleInput *g_winport_con_in = nullptr;
 const wchar_t *g_winport_backend = L"";

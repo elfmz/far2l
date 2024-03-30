2b95b516f96feaca39bccd87f00b0f2c6b3330cc
akruphi <92621645+akruphi@users.noreply.github.com>
2024-03-30 13:41:53 +0300

    NetRocks CtrlA: show non-editable owner & group + octal + rearrange
    1) show non-editable owner & group - idea from #2113
    2) show octal - idea from #2011
    3) not set on recursive checkbox by initial for directories
    4) rearrange permissions more compact a la far3 NetBox

--- NetRocks/src/UI/Activities/ConfirmChangeMode.h
+++ NetRocks/src/UI/Activities/ConfirmChangeMode.h
@@ -20,12 +20,21 @@ class ConfirmChangeMode : protected BaseDialog
 	int _i_mode_set_gid = -1;
 	int _i_mode_sticky = -1;
 
+	int _i_octal = -1;
+
 	int _i_proceed = -1, _i_cancel = -1;
 
 	void StateFromModes(int ctl, bool recurse, mode_t mode_all, mode_t mode_any, mode_t bit);
 	void StateToModes(int ctl, mode_t &mode_set, mode_t &mode_clear, mode_t bit);
+
+	char GetBitCharFromModeCheckBoxes(int _i1, int _i2, int _i3);
+	void CalcBitsCharFromModeCheckBoxes();
+
+	virtual LONG_PTR DlgProc(int msg, int param1, LONG_PTR param2);
 public:
-	ConfirmChangeMode(int selected_count, const std::string &display_path, const std::string &link_target, bool may_recurse, mode_t mode_all, mode_t mode_any);
+	ConfirmChangeMode(int selected_count, const std::string &display_path, const std::string &link_target,
+		const std::wstring &owner, const std::wstring &group,
+		bool may_recurse, mode_t mode_all, mode_t mode_any);
 
 	bool Ask(bool &recurse, mode_t &mode_set, mode_t &mode_clear);
 };

f1c2f8622b7e20a6781614d8a015b9736385a077
elfmz <fenix1905@tut.by>
2024-04-13 20:15:10 +0300

    fix version bump script

--- packaging/version_bump_patch.sh
+++ packaging/version_bump_patch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 NEW_VERSION=$1
-if [ $NEW_VERSION = '' ]; then
+if [ "$NEW_VERSION" = "" ]; then
 	NEW_VERSION=$(perl -F'\.' -lane 'print $F[0].".".$F[1].".".($F[2]+1)' ./version)
 fi
 echo NEW_VERSION=$NEW_VERSION

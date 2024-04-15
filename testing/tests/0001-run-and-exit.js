0ea56d5d88ce59676e66f12d272c3d5cd1f28d59
elfmz <fenix1905@tut.by>
2024-04-16 00:34:58 +0300

    cosmetics

--- testing/tests/0001-run-and-exit.js
+++ testing/tests/0001-run-and-exit.js
@@ -3,20 +3,20 @@ profile=mydir + "/profile"
 left=mydir + "/left-fgdfgfd"
 right=mydir + "/right"
 MkdirsAll([profile, left, right], 0700)
+
+///////////////////
+// First start - skip Help window, press F10 expecting exit confirmation dialog
 StartApp(["--tty", "--nodetect", "--mortal", "-u", profile, "-cd", left, "-cd", right]);
 ExpectString("left-fgdfgfd", 0, 0, -1, -1, 10000);
 ExpectString("Help - FAR2L", 0, 0, -1, -1, 10000);
 TypeEscape(10)
 status = AppStatus();
-cell = ReadCell(0, 0);
-cell = ReadCell(0, status.Height - 2);
-lines = SurroundedLines(1, 1, "║═│─", " \t")
-lines = SurroundedLines(status.Width - 2, 1, "║═│─", " \t")
 TypeFKey(10)
 ExpectString("Do you want to quit FAR?", 0, 0, -1, -1, 10000)
 TypeEnter()
 ExpectAppExit(0, 10000)
 
+///////////////////
 // Second start - there should no Help appeared automatically
 StartApp(["--tty", "--nodetect", "--mortal", "-u", profile, "-cd", left, "-cd", right]);
 ExpectString("left-fgdfgfd", 0, 0, -1, -1, 10000);
@@ -52,7 +52,8 @@ ExpectNoString("══ Save setup ══", 0, 0, -1, -1, 10000);
 TypeFKey(10)
 ExpectAppExit(0, 10000)
 
-// Now lets disable exit confirmation and save settings
+///////////////////
+// Third run just start and exit expecting no confirmation as such settings were saved
 StartApp(["--tty", "--nodetect", "--mortal", "-u", profile, "-cd", left, "-cd", right]);
 ExpectString("left-fgdfgfd", 0, 0, -1, -1, 10000);
 // exit without confirmation again

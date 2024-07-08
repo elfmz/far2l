c00a050f5ccbd9f2aa12c8b7a91bd31a5a95a6fe
Ivan <spnethw@gmail.com>
2024-07-08 21:54:16 +0400

    adapt FileFilterParams to work with case(in)sensitive file masks

--- far2l/src/filefilterparams.hpp
+++ far2l/src/filefilterparams.hpp
@@ -88,6 +88,7 @@ private:
 		bool Used;
 		FARString strMask;
 		CFileMask FilterMask;	// Хранилище скомпилированной маски.
+		bool ignorecase;
 	} FMask;
 
 	struct
@@ -138,7 +139,7 @@ public:
 	const FileFilterParams &operator=(const FileFilterParams &FF);
 
 	void SetTitle(const wchar_t *Title);
-	void SetMask(bool Used, const wchar_t *Mask);
+	void SetMask(bool Used, const wchar_t *Mask, bool ignorecase = true);
 	void SetDate(bool Used, DWORD DateType, FILETIME DateAfter, FILETIME DateBefore, bool bRelative);
 	void SetSize(bool Used, const wchar_t *SizeAbove, const wchar_t *SizeBelow);
 	void SetAttr(bool Used, DWORD AttrSet, DWORD AttrClear);

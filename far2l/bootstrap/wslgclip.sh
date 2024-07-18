f42f53672a9d02d7116cb4c997861291d84d05d2
Ivan Sorokin <unxed@mail.ru>
2024-07-19 00:17:09 +0200

    finnaly made it work 3/2 :))

--- far2l/bootstrap/wslgclip.sh
+++ far2l/bootstrap/wslgclip.sh
@@ -5,11 +5,15 @@ script_path=$(dirname "$(readlink -f "$0")")
 
 case "$1" in
 get)
-    if command -v cscript.exe >/dev/null 2>&1; then
-        cscript.exe //Nologo //u $(wslpath -w "$script_path"/wslgclip.vbs) | iconv -f utf-16le -t utf-8
-    else
-        powershell.exe -Command Get-Clipboard
-    fi
+    cscript.exe //Nologo //u $(wslpath -w "$script_path"/wslgclip.vbs) | iconv -f utf-16le -t utf-8
+
+#   powershell method is slower, also it have unsolved charset problems. disabled for now
+
+#    if command -v cscript.exe >/dev/null 2>&1; then
+#        cscript.exe //Nologo //u $(wslpath -w "$script_path"/wslgclip.vbs) | iconv -f utf-16le -t utf-8
+#    else
+#        powershell.exe -Command Get-Clipboard
+#    fi
 ;;
 set)
     CONTENT="$(cat)"

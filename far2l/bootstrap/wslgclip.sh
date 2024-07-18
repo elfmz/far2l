f8587400c390d8c1067c46c0449a7a7a7e4e2414
Ivan Sorokin <unxed@mail.ru>
2024-07-18 23:38:21 +0200

    revert to master for now

--- far2l/bootstrap/wslgclip.sh
+++ far2l/bootstrap/wslgclip.sh
@@ -5,19 +5,20 @@ script_path=$(dirname "$(readlink -f "$0")")
 
 case "$1" in
 get)
-#    if command -v cscript.exe >/dev/null 2>&1; then
-#        cscript.exe //Nologo $(wslpath -w "$script_path"/wslgclip.vbs)
-#    else
-#        powershell.exe -Command Get-Clipboard
-#    fi
-    powershell.exe -Command Get-Clipboard
+    if command -v cscript.exe >/dev/null 2>&1; then
+        cscript.exe //Nologo $(wslpath -w "$script_path"/wslgclip.vbs)
+    else
+        powershell.exe -Command Get-Clipboard
+    fi
+
 ;;
 set)
-    CONTENT="$(cat)"
-    echo -n "$CONTENT" | iconv -f utf-8 -t utf-16le | clip.exe
+    CONTENT=$(cat)
+    echo -n "$CONTENT" | clip.exe
     echo -n "$CONTENT"
 ;;
 "")
     (far2l --clipboard=$(readlink -f $0) >/dev/null 2>&1 &)
 ;;
 esac
+

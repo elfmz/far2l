67d92a79a9527a81b8b429d55f909deda4ceae32
Ivan Sorokin <unxed@mail.ru>
2024-07-30 22:36:09 +0200

    In graphic desktop environments Alt+Tab will be intercepted by DE to be used for switching between windows. In the same time, in kernel console this key combination is not used for any system purpose. And in the same time you can not use Alt+F1 and Alt+F2 to swtich paths on panels as those key combinations switch virtual consoles. So why not to use Alt+Tab to display Location menu for current panel? Much better UX

--- far2l/src/filepanels.cpp
+++ far2l/src/filepanels.cpp
@@ -590,6 +590,11 @@ int FilePanels::ProcessKey(FarKey Key)
 			текущим для системы после смены диска может быть каталог и на пассивной
 			панели
 		*/
+		case (KEY_ALT | KEY_TAB): {
+			ActivePanel->ChangeDisk();
+
+			break;
+		}
 		case KEY_ALTF1: {
 			LeftPanel->ChangeDisk();
 

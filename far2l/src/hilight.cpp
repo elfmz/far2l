7b5fb8e330ba75e7cfabc5c4c752ed6bfe2b8308
elfmz <fenix1905@tut.by>
2024-09-16 20:55:44 +0300

    cosmetic

--- far2l/src/hilight.cpp
+++ far2l/src/hilight.cpp
@@ -1063,29 +1063,13 @@ void HighlightFiles::SaveHiData()
 
 static bool operator==(const HighlightDataColor &hl1, const HighlightDataColor &hl2)
 {
-#if 0
-	return !memcmp(&hl1, &hl2, sizeof(HighlightDataColor));
-#else
-	if (hl1.MarkLen != hl2.MarkLen)
-		return false;
-	if (hl1.Flags != hl2.Flags)
+	if (hl1.Flags != hl2.Flags || hl1.MarkLen != hl2.MarkLen)
 		return false;
 
-	if (hl1.MarkLen)
-		if (memcmp(&hl1.Mark[0], &hl2.Mark[0], sizeof(wchar_t) * hl1.MarkLen))
-			return false;
+	if (hl1.MarkLen && wmemcmp(&hl1.Mark[0], &hl2.Mark[0], hl1.MarkLen) != 0)
+		return false;
 
-	for (size_t i = 0; i < ARRAYSIZE(hl1.Color); ++i) {
-		for (size_t j = 0; j < ARRAYSIZE(hl1.Color[i]); ++j) {
-			if (hl1.Color[i][j] != hl2.Color[i][j])
-				return false;
-			if (hl1.Mask[i][j] != hl2.Mask[i][j])
-				return false;
-		}
-	}
-
-	return true;
-#endif
+	return (hl1.Color == hl2.Color && hl1.Mask == hl2.Mask);
 }
 
 

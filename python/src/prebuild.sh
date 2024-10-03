3b7681380e87b9341b2ffef9db662d3ce383d6fa
m32 <grzegorz.makarewicz@gmail.com>
2024-10-03 04:55:58 +0200

    remove python3-venv dependency, avoid using python3-venv and use only system preprocessor

--- python/src/prebuild.sh
+++ python/src/prebuild.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
 set -e
-echo "XBUILD=$*"
 
 SRC="$1"
 DST="$2"
-PYTHON="$3"
-PREPROCESSOR="$4 -E -P -xc"
+PREPROCESSOR="$3 -E -P -xc"
 
-mkdir -p "$DST/incpy"
-
-if [ ! -f "$DST/python/.prepared" ]; then
-    echo "Preparing python virtual env at $DST/python using $PYTHON"
-    mkdir -p "$DST/python"
-    $PYTHON -m venv --system-site-packages "$DST/python"
-    "$DST/python/bin/python" -m pip install pcpp
-    $PREPROCESSOR "$SRC/python/src/consts.gen" | sh > "${DST}/incpy/consts.h"
-    echo "1" > "$DST/python/.prepared"
-fi
-
-###################
-
-cp -f -R \
-	"$SRC/python/configs/plug/far2l/"* \
-	"$DST/incpy/"
-
-"$DST/python/bin/python" "$SRC/python/src/pythongen.py" "${SRC}" "${DST}/incpy"
+echo XXX $PREPROCESSOR -I"$SRC/far2l/far2sdk" -I"$SRC/WinPort" "$SRC/python/src/far2lcffi.h" "$DST"
+$PREPROCESSOR -I"$SRC/far2l/far2sdk" -I"$SRC/WinPort" "$SRC/python/src/far2lcffi.h" > "$DST"
+cat "$SRC/python/src/far2lcffidefs.h" >> "$DST"

c44f247e191bb6e7828adc63d8f401390878a0aa
m32 <grzegorz.makarewicz@gmail.com>
2024-10-03 05:00:18 +0200

    use only system preprocessor

---
+++ python/src/far2lcffidefs.h
@@ -0,0 +1,351 @@
+// WinCompat.h
+
+#define CONSOLE_FULLSCREEN_MODE 1
+#define CONSOLE_WINDOWED_MODE 2
+
+//
+// ControlKeyState flags
+//
+
+#define RIGHT_ALT_PRESSED     0x0001 // the right alt key is pressed.
+#define LEFT_ALT_PRESSED      0x0002 // the left alt key is pressed.
+#define RIGHT_CTRL_PRESSED    0x0004 // the right ctrl key is pressed.
+#define LEFT_CTRL_PRESSED     0x0008 // the left ctrl key is pressed.
+#define SHIFT_PRESSED         0x0010 // the shift key is pressed.
+#define NUMLOCK_ON            0x0020 // the numlock light is on.
+#define SCROLLLOCK_ON         0x0040 // the scrolllock light is on.
+#define CAPSLOCK_ON           0x0080 // the capslock light is on.
+#define ENHANCED_KEY          0x0100 // the key is enhanced.
+
+#define FROM_LEFT_1ST_BUTTON_PRESSED    0x0001
+#define RIGHTMOST_BUTTON_PRESSED        0x0002
+#define FROM_LEFT_2ND_BUTTON_PRESSED    0x0004
+#define FROM_LEFT_3RD_BUTTON_PRESSED    0x0008
+#define FROM_LEFT_4TH_BUTTON_PRESSED    0x0010
+
+#define MOUSE_MOVED   0x0001
+#define DOUBLE_CLICK  0x0002
+#define MOUSE_WHEELED 0x0004
+#define MOUSE_HWHEELED 0x0008
+
+#define KEY_EVENT         0x0001 // Event contains key event record
+#define MOUSE_EVENT       0x0002 // Event contains mouse event record
+#define WINDOW_BUFFER_SIZE_EVENT 0x0004 // Event contains window change event record
+#define MENU_EVENT 0x0008 // Event contains menu event record
+#define FOCUS_EVENT 0x0010 // event contains focus change
+#define BRACKETED_PASTE_EVENT 0x0020 // event contains bracketed paste state change
+#define CALLBACK_EVENT 0x0040 // callback to be invoked when its record dequeued, its translated into NOOP_EVENT when invoked
+#define NOOP_EVENT 0x0080 // nothing interesting, typically injected to kick events dispatcher
+
+/*
+ * Virtual Keys, Standard Set
+ */
+#define VK_LBUTTON        0x01
+#define VK_RBUTTON        0x02
+#define VK_CANCEL         0x03
+#define VK_MBUTTON        0x04    /* NOT contiguous with L & RBUTTON */
+
+#define VK_XBUTTON1       0x05    /* NOT contiguous with L & RBUTTON */
+#define VK_XBUTTON2       0x06    /* NOT contiguous with L & RBUTTON */
+
+/*
+ * 0x07 : unassigned
+ */
+
+#define VK_BACK           0x08
+#define VK_TAB            0x09
+
+/*
+ * 0x0A - 0x0B : reserved
+ */
+
+#define VK_CLEAR          0x0C
+#define VK_RETURN         0x0D
+
+#define VK_SHIFT          0x10
+#define VK_CONTROL        0x11
+#define VK_MENU           0x12
+#define VK_PAUSE          0x13
+#define VK_CAPITAL        0x14
+
+#define VK_KANA           0x15
+#define VK_HANGEUL        0x15  /* old name - should be here for compatibility */
+#define VK_HANGUL         0x15
+#define VK_JUNJA          0x17
+#define VK_FINAL          0x18
+#define VK_HANJA          0x19
+#define VK_KANJI          0x19
+
+#define VK_ESCAPE         0x1B
+
+#define VK_CONVERT        0x1C
+#define VK_NONCONVERT     0x1D
+#define VK_ACCEPT         0x1E
+#define VK_MODECHANGE     0x1F
+
+#define VK_SPACE          0x20
+#define VK_PRIOR          0x21
+#define VK_NEXT           0x22
+#define VK_END            0x23
+#define VK_HOME           0x24
+#define VK_LEFT           0x25
+#define VK_UP             0x26
+#define VK_RIGHT          0x27
+#define VK_DOWN           0x28
+#define VK_SELECT         0x29
+#define VK_PRINT          0x2A
+#define VK_EXECUTE        0x2B
+#define VK_SNAPSHOT       0x2C
+#define VK_INSERT         0x2D
+#define VK_DELETE         0x2E
+#define VK_HELP           0x2F
+
+/*
+ * VK_0 - VK_9 are the same as ASCII '0' - '9' (0x30 - 0x39)
+ * 0x40 : unassigned
+ * VK_A - VK_Z are the same as ASCII 'A' - 'Z' (0x41 - 0x5A)
+ */
+
+#define VK_LWIN           0x5B
+#define VK_RWIN           0x5C
+#define VK_APPS           0x5D
+
+/*
+ * 0x5E : reserved
+ */
+
+#define VK_SLEEP          0x5F
+
+#define VK_NUMPAD0        0x60
+#define VK_NUMPAD1        0x61
+#define VK_NUMPAD2        0x62
+#define VK_NUMPAD3        0x63
+#define VK_NUMPAD4        0x64
+#define VK_NUMPAD5        0x65
+#define VK_NUMPAD6        0x66
+#define VK_NUMPAD7        0x67
+#define VK_NUMPAD8        0x68
+#define VK_NUMPAD9        0x69
+#define VK_MULTIPLY       0x6A
+#define VK_ADD            0x6B
+#define VK_SEPARATOR      0x6C
+#define VK_SUBTRACT       0x6D
+#define VK_DECIMAL        0x6E
+#define VK_DIVIDE         0x6F
+#define VK_F1             0x70
+#define VK_F2             0x71
+#define VK_F3             0x72
+#define VK_F4             0x73
+#define VK_F5             0x74
+#define VK_F6             0x75
+#define VK_F7             0x76
+#define VK_F8             0x77
+#define VK_F9             0x78
+#define VK_F10            0x79
+#define VK_F11            0x7A
+#define VK_F12            0x7B
+#define VK_F13            0x7C
+#define VK_F14            0x7D
+#define VK_F15            0x7E
+#define VK_F16            0x7F
+#define VK_F17            0x80
+#define VK_F18            0x81
+#define VK_F19            0x82
+#define VK_F20            0x83
+#define VK_F21            0x84
+#define VK_F22            0x85
+#define VK_F23            0x86
+#define VK_F24            0x87
+
+/*
+ * 0x88 - 0x8F : unassigned
+ */
+
+#define VK_NUMLOCK        0x90
+#define VK_SCROLL         0x91
+
+/*
+ * NEC PC-9800 kbd definitions
+ */
+#define VK_OEM_NEC_EQUAL  0x92   // '=' key on numpad
+
+/*
+ * Fujitsu/OASYS kbd definitions
+ */
+#define VK_OEM_FJ_JISHO   0x92   // 'Dictionary' key
+#define VK_OEM_FJ_MASSHOU 0x93   // 'Unregister word' key
+#define VK_OEM_FJ_TOUROKU 0x94   // 'Register word' key
+#define VK_OEM_FJ_LOYA    0x95   // 'Left OYAYUBI' key
+#define VK_OEM_FJ_ROYA    0x96   // 'Right OYAYUBI' key
+
+/*
+ * 0x97 - 0x9F : unassigned
+ */
+
+/*
+ * VK_L* & VK_R* - left and right Alt, Ctrl and Shift virtual keys.
+ * Used only as parameters to GetAsyncKeyState() and GetKeyState().
+ * No other API or message will distinguish left and right keys in this way.
+ */
+#define VK_LSHIFT         0xA0
+#define VK_RSHIFT         0xA1
+#define VK_LCONTROL       0xA2
+#define VK_RCONTROL       0xA3
+#define VK_LMENU          0xA4
+#define VK_RMENU          0xA5
+
+#define VK_BROWSER_BACK        0xA6
+#define VK_BROWSER_FORWARD     0xA7
+#define VK_BROWSER_REFRESH     0xA8
+#define VK_BROWSER_STOP        0xA9
+#define VK_BROWSER_SEARCH      0xAA
+#define VK_BROWSER_FAVORITES   0xAB
+#define VK_BROWSER_HOME        0xAC
+
+#define VK_VOLUME_MUTE         0xAD
+#define VK_VOLUME_DOWN         0xAE
+#define VK_VOLUME_UP           0xAF
+#define VK_MEDIA_NEXT_TRACK    0xB0
+#define VK_MEDIA_PREV_TRACK    0xB1
+#define VK_MEDIA_STOP          0xB2
+#define VK_MEDIA_PLAY_PAUSE    0xB3
+#define VK_LAUNCH_MAIL         0xB4
+#define VK_LAUNCH_MEDIA_SELECT 0xB5
+#define VK_LAUNCH_APP1         0xB6
+#define VK_LAUNCH_APP2         0xB7
+
+/*
+ * 0xB8 - 0xB9 : reserved
+ */
+
+#define VK_OEM_1          0xBA   // ';:' for US
+#define VK_OEM_PLUS       0xBB   // '+' any country
+#define VK_OEM_COMMA      0xBC   // ',' any country
+#define VK_OEM_MINUS      0xBD   // '-' any country
+#define VK_OEM_PERIOD     0xBE   // '.' any country
+#define VK_OEM_2          0xBF   // '/?' for US
+#define VK_OEM_3          0xC0   // '`~' for US
+
+/*
+ * 0xC1 - 0xD7 : reserved
+ */
+
+/*
+ * 0xD8 - 0xDA : unassigned
+ */
+
+#define VK_OEM_4          0xDB  //  '[{' for US
+#define VK_OEM_5          0xDC  //  '\|' for US
+#define VK_OEM_6          0xDD  //  ']}' for US
+#define VK_OEM_7          0xDE  //  ''"' for US
+#define VK_OEM_8          0xDF
+
+/*
+ * 0xE0 : reserved
+ */
+
+/*
+ * Various extended or enhanced keyboards
+ */
+#define VK_OEM_AX         0xE1  //  'AX' key on Japanese AX kbd
+#define VK_OEM_102        0xE2  //  "<>" or "\|" on RT 102-key kbd.
+#define VK_ICO_HELP       0xE3  //  Help key on ICO
+#define VK_ICO_00         0xE4  //  00 key on ICO
+
+#define VK_PROCESSKEY     0xE5
+
+#define VK_ICO_CLEAR      0xE6
+
+
+#define VK_PACKET         0xE7
+
+#define VK_UNASSIGNED     0xE8
+
+/*
+ * 0xE8 : unassigned
+ */
+
+/*
+ * Nokia/Ericsson definitions
+ */
+#define VK_OEM_RESET      0xE9
+#define VK_OEM_JUMP       0xEA
+#define VK_OEM_PA1        0xEB
+#define VK_OEM_PA2        0xEC
+#define VK_OEM_PA3        0xED
+#define VK_OEM_WSCTRL     0xEE
+#define VK_OEM_CUSEL      0xEF
+#define VK_OEM_ATTN       0xF0
+#define VK_OEM_FINISH     0xF1
+#define VK_OEM_COPY       0xF2
+#define VK_OEM_AUTO       0xF3
+#define VK_OEM_ENLW       0xF4
+#define VK_OEM_BACKTAB    0xF5
+
+#define VK_ATTN           0xF6
+#define VK_CRSEL          0xF7
+#define VK_EXSEL          0xF8
+#define VK_EREOF          0xF9
+#define VK_PLAY           0xFA
+#define VK_ZOOM           0xFB
+#define VK_NONAME         0xFC
+#define VK_PA1            0xFD
+#define VK_OEM_CLEAR      0xFE
+
+#define KEYEVENTF_EXTENDEDKEY 0x0001
+#define KEYEVENTF_KEYUP       0x0002
+#define KEYEVENTF_UNICODE     0x0004
+#define KEYEVENTF_SCANCODE    0x0008
+
+#define TICKS_PER_SECOND        10000000
+#define EPOCH_DIFFERENCE        11644473600
+
+#define FILE_ATTRIBUTE_READONLY         0x00000001
+#define FILE_ATTRIBUTE_HIDDEN           0x00000002
+#define FILE_ATTRIBUTE_SYSTEM           0x00000004
+#define FILE_ATTRIBUTE_DIRECTORY        0x00000010
+#define FILE_ATTRIBUTE_ARCHIVE          0x00000020
+#define FILE_ATTRIBUTE_DEVICE           0x00000040
+#define FILE_ATTRIBUTE_NORMAL           0x00000080
+#define FILE_ATTRIBUTE_TEMPORARY        0x00000100
+#define FILE_ATTRIBUTE_SPARSE_FILE      0x00000200
+#define FILE_ATTRIBUTE_REPARSE_POINT    0x00000400
+#define FILE_ATTRIBUTE_COMPRESSED       0x00000800
+#define FILE_ATTRIBUTE_OFFLINE          0x00001000
+#define FILE_ATTRIBUTE_NOT_CONTENT_INDEXED 0x00002000
+#define FILE_ATTRIBUTE_ENCRYPTED        0x00004000
+#define FILE_ATTRIBUTE_INTEGRITY_STREAM 0x00008000
+#define FILE_ATTRIBUTE_VIRTUAL          0x00010000
+#define FILE_ATTRIBUTE_NO_SCRUB_DATA    0x00020000
+#define FILE_ATTRIBUTE_BROKEN           0x00200000
+#define FILE_ATTRIBUTE_EXECUTABLE       0x00400000
+#define FILE_ATTRIBUTE_DEVICE_CHAR          0x00800000
+#define FILE_ATTRIBUTE_DEVICE_FIFO          0x01000000
+#define FILE_ATTRIBUTE_DEVICE_SOCK          0x02000000
+#define FILE_ATTRIBUTE_HARDLINKS            0x08000000
+
+// WinPort.h
+
+#define CFKP_KEEP_MATCHED_KEY_EVENTS    0x001
+#define CFKP_KEEP_UNMATCHED_KEY_EVENTS  0x002
+#define CFKP_KEEP_MOUSE_EVENTS          0x004
+#define CFKP_KEEP_OTHER_EVENTS          0x100
+
+#define EXCLUSIVE_CTRL_LEFT			0x00000001
+#define EXCLUSIVE_CTRL_RIGHT		0x00000002
+#define EXCLUSIVE_ALT_LEFT			0x00000004
+#define EXCLUSIVE_ALT_RIGHT			0x00000008
+#define EXCLUSIVE_WIN_LEFT			0x00000010
+#define EXCLUSIVE_WIN_RIGHT			0x00000020
+
+#define CONSOLE_PAINT_SHARP			0x00010000
+#define CONSOLE_OSC52CLIP_SET		0x00020000
+
+#define CONSOLE_TTY_PALETTE_OVERRIDE	0x00040000
+
+#define TWEAK_STATUS_SUPPORT_EXCLUSIVE_KEYS	0x01
+#define TWEAK_STATUS_SUPPORT_PAINT_SHARP	0x02
+#define TWEAK_STATUS_SUPPORT_OSC52CLIP_SET	0x04
+#define TWEAK_STATUS_SUPPORT_CHANGE_FONT	0x08
+#define TWEAK_STATUS_SUPPORT_TTY_PALETTE	0x10
+#define TWEAK_STATUS_SUPPORT_BLINK_RATE		0x20

9b6fc5be97967cbe597452db2a7bd8bedeabab38
Ivan Sorokin <unxed@mail.ru>
2024-10-24 11:21:21 +0200

    comments

--- WinPort/src/Backend/WX/wxMain.cpp
+++ WinPort/src/Backend/WX/wxMain.cpp
@@ -1304,8 +1304,9 @@ void WinPortPanel::OnKeyDown( wxKeyEvent& event )
 	// also it didnt cause problems yet
 	if ( (_key_tracker.Shift() && !event.ShiftDown())
 		|| ((_key_tracker.LeftControl() || _key_tracker.RightControl()) && !event.ControlDown())) {
-		if ((!_key_tracker.Alt() || _key_tracker.Shift() || g_wayland) && _key_tracker.CheckForSuddenModifiersUp()) {
-			_exclusive_hotkeys.Reset();
+		if ((!_key_tracker.Alt() || _key_tracker.Shift() || g_wayland) && // workaround for #2294, 2464
+			_key_tracker.CheckForSuddenModifiersUp()) {
+				_exclusive_hotkeys.Reset();
 		}
 	}
 
@@ -1341,7 +1342,7 @@ void WinPortPanel::OnKeyDown( wxKeyEvent& event )
 
 	if ( (dwMods != 0 && event.GetUnicodeKey() < 32)
 		|| ( (dwMods & (LEFT_CTRL_PRESSED | RIGHT_CTRL_PRESSED | LEFT_ALT_PRESSED)) &&
-			(!LEFT_ALT_PRESSED || !event.GetUnicodeKey()) ) // workaround for #2294, 2464, wx issue #23421
+			(!LEFT_ALT_PRESSED || !event.GetUnicodeKey()) ) // workaround for wx issue #23421
 		|| event.GetKeyCode() == WXK_DELETE || event.GetKeyCode() == WXK_RETURN
 		|| (event.GetUnicodeKey()==WXK_NONE && !IsForcedCharTranslation(event.GetKeyCode()) ))
 	{
@@ -1440,13 +1441,14 @@ void WinPortPanel::OnKeyUp( wxKeyEvent& event )
 			ir.Event.KeyEvent.bKeyDown = TRUE;
 		}
 #endif
-		if (!LEFT_ALT_PRESSED || !event.GetUnicodeKey()) { // // workaround for #2294, 2464, wx issue #23421
+		if (!LEFT_ALT_PRESSED || !event.GetUnicodeKey()) { // workaround for wx issue #23421
 
 			wxConsoleInputShim::Enqueue(&ir, 1);
 		}
 	}
-	if ((!_key_tracker.Alt() || _key_tracker.Shift() || g_wayland) && _key_tracker.CheckForSuddenModifiersUp()) {
-		_exclusive_hotkeys.Reset();
+	if ((!_key_tracker.Alt() || _key_tracker.Shift() || g_wayland) && // workaround for #2294, 2464
+		_key_tracker.CheckForSuddenModifiersUp()) {
+			_exclusive_hotkeys.Reset();
 	}
 	//event.Skip();
 }
@@ -1494,7 +1496,7 @@ void WinPortPanel::OnChar( wxKeyEvent& event )
 
 		if (event.AltDown()) {
 
-			// // workaround for #2294, 2464, wx issue #23421
+			// workaround for wx issue #23421
 
 			// OnChar KeyCode value is empty for non-latin letters in older wx so let's use value from previous KeyDown
 			// See wx issue #23379 for details

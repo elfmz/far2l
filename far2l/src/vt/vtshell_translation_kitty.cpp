092d1d36aec50a23eafd698412093d799ee243da
Ivan Sorokin <unxed@mail.ru>
2025-01-27 22:07:54 +0100

    minor

--- far2l/src/vt/vtshell_translation_kitty.cpp
+++ far2l/src/vt/vtshell_translation_kitty.cpp
@@ -112,8 +112,8 @@ std::string VT_TranslateKeyToKitty(const KEY_EVENT_RECORD &KeyEvent, int flags)
 			((KeyEvent.wVirtualKeyCode == VK_RETURN) && (ctrl|alt|shift)) ||
 			((KeyEvent.wVirtualKeyCode == VK_TAB)    && (ctrl|alt|shift)) ||
 			((KeyEvent.wVirtualKeyCode == VK_BACK)   && (ctrl|alt|shift)) ||
-			((KeyEvent.wVirtualKeyCode == VK_SPACE)  && (ctrl|alt)) ||
-			(KeyEvent.uChar.UnicodeChar && (ctrl|alt))
+			((KeyEvent.wVirtualKeyCode == VK_SPACE)  && (ctrl|alt))
+//			(KeyEvent.uChar.UnicodeChar && (ctrl|alt)) // redundancy
 		))
 	);
 

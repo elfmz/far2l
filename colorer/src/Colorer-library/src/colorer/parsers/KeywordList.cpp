0804b847132a4b4e846c58898fd808e38d5be327
Aleksey Dobrunov <ctapmex@ctapmex.com>
2025-05-10 21:20:30 +0500

    update colorer_lib
    Update colorer_lib to latest - major fix work with keywords

--- colorer/src/Colorer-library/src/colorer/parsers/KeywordList.cpp
+++ colorer/src/Colorer-library/src/colorer/parsers/KeywordList.cpp
@@ -44,16 +44,29 @@ void KeywordList::sortList()
 void KeywordList::substrIndex()
 {
   for (int i = count - 1; i > 0; i--) {
-    for (int ii = i - 1; ii != 0; ii--) {
-      if ((*kwList[ii].keyword)[0] != (*kwList[i].keyword)[0]) {
-        break;
+    for (int ii = i - 1; ii >= 0; ii--) {
+      if (matchCase) {
+        if ((*kwList[ii].keyword)[0] != (*kwList[i].keyword)[0]) {
+          break;
+        }
+        if (kwList[ii].keyword->length() < kwList[i].keyword->length() &&
+            UnicodeString(*kwList[i].keyword, 0, kwList[ii].keyword->length()).compare(*kwList[ii].keyword) == 0)
+        {
+          kwList[i].indexOfShorter = ii;
+          break;
+        }
       }
-      if (kwList[ii].keyword->length() < kwList[i].keyword->length() &&
-          UnicodeString(*kwList[i].keyword, 0, kwList[ii].keyword->length())
-                  .compare(*kwList[ii].keyword) == 0)
-      {
-        kwList[i].indexOfShorter = ii;
-        break;
+      else {
+        if (Character::toLowerCase((*kwList[ii].keyword)[0]) != Character::toLowerCase((*kwList[i].keyword)[0])) {
+          break;
+        }
+        if (kwList[ii].keyword->length() < kwList[i].keyword->length() &&
+            UStr::caseCompare(UnicodeString(*kwList[i].keyword, 0, kwList[ii].keyword->length()),
+                              *kwList[ii].keyword) == 0)
+        {
+          kwList[i].indexOfShorter = ii;
+          break;
+        }
       }
     }
   }

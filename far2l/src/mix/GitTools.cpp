97ed1e34300579370c0ec5c23ed9eace8da3ff64
Dmytro Ovdiienko <dmytro.ovdiienko@iongroup.com>
2025-05-17 19:30:28 +0200

    FIX: Minor improvements

--- far2l/src/mix/GitTools.cpp
+++ far2l/src/mix/GitTools.cpp
@@ -4,26 +4,39 @@
 #if defined(USELIBGIT2)
 #include <codecvt>
 #include <locale>
+#include <string_view>
+#include <filesystem>
 
 #include <git2.hpp>
 
-static std::wstring_convert<std::codecvt_utf8<wchar_t>> Utf8Conv;
 static git2::environment GitEnvironment;
 
 #define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 
-static std::filesystem::path makeStdPath(FARString const &path)
+#if defined(__GNUC__) && !defined(__clang__) && (GCC_VERSION > 90100 && GCC_VERSION < 110500)
+
+//
+// Workaround for the GCC bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=95048
+//
+
+static std::wstring_convert<std::codecvt_utf8<wchar_t>> Utf8Conv;
+
+static std::filesystem::path MakeStdPath(FARString const &path)
 {
     auto pathStrView = std::wstring_view{path.CPtr(), static_cast<size_t>(path.CEnd() - path.CPtr())};
 
-#if defined(__GNUC__) && !defined(__clang__) && (GCC_VERSION > 90100 && GCC_VERSION < 110500)
-    // Workaround for the GCC bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=95048
     auto pathStrUtf8 = Utf8Conv.to_bytes(pathStrView.begin(), pathStrView.end());
     return std::filesystem::path{std::move(pathStrUtf8)};
-#else
-    return std::filesystem::path{pathStrView};
-#endif
 }
+#else
+
+static std::filesystem::path MakeStdPath(FARString const &path)
+{
+    auto pathStrView = std::wstring_view{path.CPtr(), static_cast<size_t>(path.CEnd() - path.CPtr())};
+
+    return std::filesystem::path{pathStrView};
+}
+#endif
 
 #endif    // USELIBGIT2
 
@@ -32,7 +45,7 @@ FARString GetGitBranchName(FARString const &path)
     try {
 #if defined(USELIBGIT2)
 
-        auto repo = git2::repository::try_discover(GitEnvironment, makeStdPath(path));
+        auto repo = git2::repository::try_discover(GitEnvironment, MakeStdPath(path));
 
         if (!repo)
             return {};

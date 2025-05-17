7cfdf97b024c26c1a32479f2aa895aa07d161ee7
Dmytro Ovdiienko <dmytro.ovdiienko@iongroup.com>
2025-05-17 18:48:49 +0200

    FIX: Extract the workaround for the GCC 11.4

--- far2l/src/mix/GitTools.cpp
+++ far2l/src/mix/GitTools.cpp
@@ -9,17 +9,30 @@
 
 static std::wstring_convert<std::codecvt_utf8<wchar_t>> Utf8Conv;
 static git2::environment GitEnvironment;
+
+#define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+
+static std::filesystem::path makeStdPath(FARString const &path)
+{
+    auto pathStrView = std::wstring_view{path.CPtr(), static_cast<size_t>(path.CEnd() - path.CPtr())};
+
+#if defined(__GNUC__) && !defined(__clang__) && (GCC_VERSION > 90100 && GCC_VERSION < 110500)
+    // Workaround for the GCC bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=95048
+    auto pathStrUtf8 = Utf8Conv.to_bytes(pathStrView.begin(), pathStrView.end());
+    return std::filesystem::path{std::move(pathStrUtf8)};
+#else
+    return std::filesystem::path{pathStrView};
+#endif
+}
+
 #endif    // USELIBGIT2
 
 FARString GetGitBranchName(FARString const &path)
 {
     try {
 #if defined(USELIBGIT2)
-        auto pathStrView = std::wstring_view{path.CPtr(), static_cast<size_t>(path.CEnd() - path.CPtr())};
-        auto pathStrUtf8 = Utf8Conv.to_bytes(pathStrView.begin(), pathStrView.end());
-        auto path = std::filesystem::path{std::move(pathStrUtf8)};
 
-        auto repo = git2::repository::try_discover(GitEnvironment, path);
+        auto repo = git2::repository::try_discover(GitEnvironment, makeStdPath(path));
 
         if (!repo)
             return {};

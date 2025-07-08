a1659f8c93654ecbd1df1dd7d609346709643c95
tempergate <kate@tempergate.co.uk>
2025-07-08 09:13:44 +0000

    Update trash.sh
     redirect stderr to stdout,

--- far2l/bootstrap/trash.sh
+++ far2l/bootstrap/trash.sh
@@ -17,10 +17,10 @@ fi
 if command -v kioclient >/dev/null 2>&1; then
 	kioclient move "$1" trash:/ 2>"$2"
 
-elif command -v trash put "$1" 2>"$2"; then
+elif command -v trash >/dev/null 2>&1; then
 	trash put "$1" 2>"$2"
 
-elif command -v trashу put "$1" 2>"$2"; then
+elif command -v trashу >/dev/null 2>&1; then
 	trashу put "$1" 2>"$2" 
 
 elif command -v gio >/dev/null 2>&1; then

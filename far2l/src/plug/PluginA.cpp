d25142badd5639f501d7bf6ed1c27c5fd4e8f693
anta999 <arcticshine999@gmail.com>
2025-09-16 01:56:28 +0400

    Add GetColor to PluginAPI

--- far2l/src/plug/PluginA.cpp
+++ far2l/src/plug/PluginA.cpp
@@ -67,8 +67,10 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "execute.hpp"
 #include "flink.hpp"
 #include "ConfigRW.hpp"
+#include "pick_color.hpp"
 #include "wrap.cpp"
 #include <KeyFileHelper.h>
+
 
 #include "farversion.h"
 
@@ -413,6 +415,7 @@ CreatePluginStartupInfoA(PluginA *pPlugin, oldfar::PluginStartupInfo *PSI, oldfa
 		StartupInfo.SendDlgMessage = FarSendDlgMessageA;
 		StartupInfo.DefDlgProc = FarDefDlgProcA;
 		StartupInfo.InputBox = FarInputBoxA;
+		StartupInfo.ColorDialog = FarColorDialogA;
 	}
 
 	*PSI = StartupInfo;

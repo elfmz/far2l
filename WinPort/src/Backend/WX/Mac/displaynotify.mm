f4934fecbcdf481fd85ff42081c62709b8c4a042
Sergey Fedorov <barracuda@macos-powerpc.org>
2025-09-17 20:50:23 +0800

    displaynotify.mm: add a fallback to fix build on < 10.8

--- WinPort/src/Backend/WX/Mac/displaynotify.mm
+++ WinPort/src/Backend/WX/Mac/displaynotify.mm
@@ -1,12 +1,24 @@
 #import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
+#include <AvailabilityMacros.h>
 
 bool MacDisplayNotify(const char *title, const char *text)
 {
+	NSString *nsTitle = [NSString stringWithUTF8String:title];
+	NSString *nsText  = [NSString stringWithUTF8String:text];
+
+#if MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
 	NSUserNotification *userNotification = [[NSUserNotification alloc] init];
-	userNotification.title = [NSString stringWithUTF8String:title];
-	userNotification.informativeText = [NSString stringWithUTF8String:text];
+	userNotification.title = nsTitle;
+	userNotification.informativeText = nsText;
 
 	[[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:userNotification]; //deliverNotification
+#else
+	NSAlert *alert = [[NSAlert alloc] init];
+	[alert setMessageText:nsTitle];
+	[alert setInformativeText:nsText];
+	[alert addButtonWithTitle:@"OK"];
+	[alert runModal];
+#endif
 	return true;
 }

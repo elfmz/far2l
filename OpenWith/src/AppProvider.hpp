ee3a156270d8b346df0ee4b48af1cc6abe57da1c
Ivan <spnethw@gmail.com>
2025-10-09 13:07:45 +0400

    OpenWith: added the ability to select multiple files

--- OpenWith/src/AppProvider.hpp
+++ OpenWith/src/AppProvider.hpp
@@ -24,16 +24,14 @@ public:
 	explicit AppProvider(TMsgGetter msg_getter) : m_GetMsg(std::move(msg_getter)) {}
 	virtual ~AppProvider() = default;
 
-	virtual std::vector<CandidateInfo> GetAppCandidates(const std::wstring& pathname) = 0;
-	virtual std::wstring GetMimeType(const std::wstring& pathname) = 0;
-	virtual std::wstring ConstructCommandLine(const CandidateInfo& candidate, const std::wstring& pathname) = 0;
-
-	virtual std::vector<Field> GetCandidateDetails(const CandidateInfo& candidate) = 0;
-
 	static std::unique_ptr<AppProvider> CreateAppProvider(TMsgGetter msg_getter);
 
-	virtual std::vector<ProviderSetting> GetPlatformSettings() { return {}; }
+	virtual std::vector<CandidateInfo> GetAppCandidates(const std::vector<std::wstring>& pathnames) = 0;
+	virtual std::vector<std::wstring> GetMimeTypes(const std::vector<std::wstring>& pathnames) = 0;
+	virtual std::vector<std::wstring> ConstructCommandLine(const CandidateInfo& candidate, const std::vector<std::wstring>& pathnames) = 0;
+	virtual std::vector<Field> GetCandidateDetails(const CandidateInfo& candidate) = 0;
 
+	virtual std::vector<ProviderSetting> GetPlatformSettings() { return {}; }
 	virtual void SetPlatformSettings(const std::vector<ProviderSetting>& settings) { }
 	virtual void LoadPlatformSettings() { }
 	virtual void SavePlatformSettings() { }

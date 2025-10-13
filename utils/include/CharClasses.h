2d10aa184e776bf74ade823f20d698c8e715af05
Alexey Shelaev <75270873+exkrexpexfex@users.noreply.github.com>
2025-10-13 03:56:35 +0100

    Handling Zero Width Joiner

--- utils/include/CharClasses.h
+++ utils/include/CharClasses.h
@@ -63,6 +63,8 @@ class CharClasses
 	}
 
 public:
+	static constexpr wchar_t VARIATION_SELECTOR_16 = 0xFE0F;
+	static constexpr wchar_t ZERO_WIDTH_JOINER = 0x200D;
 	inline CharClasses(wchar_t c) : _c(c) {}
 
 	static void InitCharFlags() {
@@ -114,7 +116,13 @@ public:
 	static inline bool IsFullWidth(const wchar_t* p) {
 		if (!p || *p <= ASCII_MAX) return false;
 		//Variation Selector-16 indicates that the previous character should be rendered as an image
-		if (*(p + 1) == 0xFE0F) return true;
+		if (*(p + 1) == VARIATION_SELECTOR_16) return true;
+		return Get(*p) & IS_FULLWIDTH;
+	}
+	static inline bool IsFullWidth(const wchar_t* p, size_t n) {
+		if (!p || *p <= ASCII_MAX) return false;
+		//Variation Selector-16 indicates that the previous character should be rendered as an image
+		if (n > 1 && *(p + 1) == VARIATION_SELECTOR_16) return true;
 		return Get(*p) & IS_FULLWIDTH;
 	}
 	static inline bool IsFullWidth(wchar_t c) {

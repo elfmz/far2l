735bd8086677248f76fb07040e45e6ddfc0cdb99
akruphi <92621645+akruphi@users.noreply.github.com>
2025-12-28 20:40:48 +0300

    Process Ctrl-A even in read-only edit controls + refactoring (from far2m)
    from
    https://github.com/shmuz/far2m/commit/fbdb2068916b460fe3c4053d3fc1f5ba9620b7cf
    https://github.com/shmuz/far2m/commit/4a2c5b64940d62f921ff3be9ddcddf7c297ae180

--- far2l/src/edit.cpp
+++ far2l/src/edit.cpp
@@ -864,6 +864,12 @@ int Edit::ProcessKey(FarKey Key)
 			Show();
 			return FALSE;
 
+		case KEY_CTRLU:
+			SetClearFlag(0);
+			Select(-1, 0);
+			Show();
+			return FALSE;
+
 		case KEY_SHIFTLEFT:
 		case KEY_SHIFTNUMPAD4: {
 			if (CurPos > 0) {

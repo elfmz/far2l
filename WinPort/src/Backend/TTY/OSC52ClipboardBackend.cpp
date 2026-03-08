c7f34c22fa04ab408ec7564f435fd3d56f8579e0
Victor Krapivin <vik@scand.com>
2026-03-08 21:40:53 +0100

    TTY: added support of clipboard and primary buffer separation for OSC52

--- WinPort/src/Backend/TTY/OSC52ClipboardBackend.cpp
+++ WinPort/src/Backend/TTY/OSC52ClipboardBackend.cpp
@@ -2,6 +2,10 @@
 #include "WinPort.h"
 #include <utils.h>
 
+#include <RandomString.h>
+#include <base64.h>
+#include <UtfConvert.hpp>
+
 OSC52ClipboardBackend::OSC52ClipboardBackend(IOSC52Interactor *interactor) :
 	_interactor(interactor)
 {
@@ -15,4 +19,49 @@ void *OSC52ClipboardBackend::OnClipboardSetData(UINT format, void *data)
 		_interactor->OSC52SetClipboard(str.c_str());
 	}
 	return FSClipboardBackend::OnClipboardSetData(format, data);
+}
+
+void *OSC52ClipboardBackend::OnClipboardGetData(UINT format) 
+{
+	// VK: first, we need to send ESC "[52;" _is _primary ? 'p' : 'c' ";?\a"
+	// then we need to wait on guard until the data is arrived; and then 
+	// decode it from base64 and return the result
+
+	fprintf(stderr, "OSC52ClipboardBackend: OnClipboardGetData\n");
+
+	const char*	stx = _interactor->OSC52RequestClipboardData(_is_primary);
+	if (!stx)
+		return FSClipboardBackend::OnClipboardGetData(format);
+
+	std::string _text(stx);
+	if (_text.empty())	
+		return nullptr;
+
+	const std::vector<unsigned char> &data	= base64_decode(_text.data(), _text.size());
+	if (data.empty()) 
+		return nullptr;
+
+	void *out;
+	if (format == CF_UNICODETEXT) {
+		size_t utf8_len = data.size();
+		while (utf8_len && !data[utf8_len - 1]) {
+			--utf8_len;
+		}
+
+		std::wstring ws;
+		MB2Wide((const char *)data.data(), utf8_len, ws);
+
+		out = WINPORT(ClipboardAlloc)( (ws.size() + 1) * sizeof(wchar_t));
+		if (out) {
+			memcpy(out, ws.c_str(), (ws.size() + 1) * sizeof(wchar_t));
+		}
+
+	} else {
+		out = WINPORT(ClipboardAlloc)(data.empty() ? 1 : data.size());
+		if (out && !data.empty()) {
+			memcpy(out, data.data(), data.size());
+		}
+	}
+
+	return out;
 }
--- WinPort/src/Backend/TTY/TTYBackend.cpp
+++ WinPort/src/Backend/TTY/TTYBackend.cpp
@@ -425,6 +425,9 @@ void TTYBackend::WriterThread()
 			if (ae.osc52clip_set) {
 				DispatchOSC52ClipSet(tty_out);
 			}
+			if (ae.osc52clip_request) {
+				DispatchOSC52ClipRequest(tty_out);
+			}
 			if (ae.images_probe) {
 				DispatchImagesProbe(tty_out);
 			}
@@ -679,7 +682,15 @@ void TTYBackend::DispatchOSC52ClipSet(TTYOutput &tty_out)
 		std::unique_lock<std::mutex> lock(_async_mutex);
 		osc52clip.swap(_osc52clip);
 	}
-	tty_out.SendOSC52ClipSet(osc52clip);
+	tty_out.SendOSC52ClipSet(osc52clip, _osc52clip_is_primary);
+}
+
+void TTYBackend::DispatchOSC52ClipRequest(TTYOutput &tty_out)
+{
+	{
+		std::unique_lock<std::mutex> lock(_async_mutex);
+	}
+	tty_out.SendOSC52ClipRequest(_osc52clip_is_primary);
 }
 
 /////////////////////////////////////////////////////////////////////////
@@ -980,12 +991,13 @@ void TTYBackend::ChooseSimpleClipboardBackend()
 	}
 }
 
-void TTYBackend::OSC52SetClipboard(const char *text)
+void TTYBackend::OSC52SetClipboard(const char *text, bool is_primary_buffer)
 {
 	fprintf(stderr, "TTYBackend::OSC52SetClipboard\n");
 	std::unique_lock<std::mutex> lock(_async_mutex);
 	_osc52clip = text;
 	_ae.osc52clip_set = true;
+	_osc52clip_is_primary = is_primary_buffer;
 	_async_cond.notify_all();
 }
 
@@ -1256,6 +1268,41 @@ void TTYBackend::OnGetCellSize(unsigned int w, unsigned int h)
 	std::lock_guard<std::mutex> lock(_async_mutex);
 	_pix_per_cell.X = w;
 	_pix_per_cell.Y = h;
+}
+
+void TTYBackend::OnOSC52PasteReply(const std::string& s, bool is_primary_buffer) 
+{
+	// vk: todo: call clipboard backend to fit the request with paste
+	fprintf(stderr, "TTYBackend: OSC52 paste arrived: %c, %ld length\n", is_primary_buffer ? 'P' : 'C', s.size() );
+	std::unique_lock<std::mutex> lock(_async_mutex);
+	_osc52clip = s;
+	_ae.osc52clip_get = true;
+	_async_cond.notify_all();
+}
+
+const char* TTYBackend::OSC52RequestClipboardData(bool is_primary_buffer) 
+{
+	// vk: todo: many terminals do not have / blocks osc52 read, so we need to handle this
+	fprintf(stderr, "TTY: OSC52RequestClipboardData request\n");
+	{
+		std::unique_lock<std::mutex> lock(_async_mutex);
+		_ae.osc52clip_request = true;
+		_async_cond.notify_all();
+	}
+
+	fprintf(stderr, "TTY: OSC52RequestClipboardData wait for response\n");
+	for(;;) {
+		std::unique_lock<std::mutex> lock(_async_mutex);
+		if(_async_cond.wait_for(lock, std::chrono::milliseconds(100))) {
+			if (_ae.osc52clip_get) break;
+		}
+		else {
+			fprintf(stderr, "TTY: OSC52RequestClipboardData timeout\n");
+			return nullptr;
+		}
+	}
+	fprintf(stderr, "TTY: OSC52RequestClipboardData response arrived\n");
+	return _osc52clip.c_str();
 }
 
 DWORD TTYBackend::QueryControlKeys()

67d2600655e7295480b51542cba15dcfe0fd9493
m32 <grzegorz.makarewicz@gmail.com>
2026-06-24 22:59:14 +0200

    return allocated pointer - must be preserved

--- python/configs/plug/far2l/fardialog.py
+++ python/configs/plug/far2l/fardialog.py
@@ -65,12 +65,16 @@ class Dialog:
         return self.f2s(sptr)
 
     def SetText(self, ID, Str):
-        sptr = self.s2f(Str)
+        if isinstance(Str, str):
+            sptr = self.s2f(Str)
+        else:
+            sptr = Str
         # preserve item.data ?
         # self.id2item[ID][1] = sptr
         self.info.SendDlgMessage(
             self.hDlg, self.ffic.DM_SETTEXTPTR, ID, self.ffi.cast("LONG_PTR", sptr)
         )
+        return sptr
 
     def GetCheck(self, ID):
         return self.info.SendDlgMessage(self.hDlg, self.ffic.DM_GETCHECK, ID, 0)

f8e798827eee358bee1e0711c920780c407ddb06
elfmz <elfmz1905@gmail.com>
2026-08-25 23:34:11 +0300

    avoid empty line printed

--- far2l/src/vt/vtshell.cpp
+++ far2l/src/vt/vtshell.cpp
@@ -377,7 +377,7 @@ class VTShell : VTOutputReader::IProcessor, VTInputReader::IProcessor, IVTShell
 		clock_t when_printed_cmd = 0;
 		MakeConsoleDisabled(true);
 		VT_ComposeMarker(_startup_marker);
-		std::string cmd = "\n ";
+		std::string cmd = " ";
 		cmd+= VT_ComposeMarkerCommand(_startup_marker);
 		cmd+= '\n';
 		StartIOReaders();
